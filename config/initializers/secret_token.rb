05352f62d1ba914ba9a3664cdca8234432c1a371
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2012-11-15 12:07:06 -0600

    version last batle de cursame

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cursame30Lb::Application.config.secret_token = '2fdfded1f1c61ec695773c4fa80f6f8d7c084e2c3c7f997c2f7319eb096164fe3da2ebaff09dd784570f2639a4edc2ae793875923ea7ab31b02f70403cf6f3dd'
