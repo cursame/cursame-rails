2310bb7fc839b8395dc440a27b4848ef9bc5e1dd
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2012-11-21 16:21:12 -0600

    subiendo modificaciones para eliminar error de loggin

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :name, :id, :personal_url
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :name, :id, :personal_url, :avatar
   
   has_many :networks_users, :dependent => :destroy
   #comentarios para usuarios
