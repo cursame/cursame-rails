e76c39018c9f8d78768f2799ffb7fa71ad1ee8db
Rodrigo Ayala <azul.rejected@gmail.com>
2012-12-17 13:54:02 -0600

    Revert "Merge branch 'development' of github.com:alfredo777/cursame-3.0-LB into development"
    This reverts commit e01ebe1e733e95814ab3068dc1d2af622d116d23, reversing
    changes made to 611c661c0fe026bef6e0704231d6e4eca0875f42.

---
+++ config/routes.rb
@@ -0,0 +1,125 @@
+Cursame30Lb::Application.routes.draw do
+  
+  
+  
+  resources :permissions
+
+  resources :roles
+
+  #manejo de users
+  
+  devise_for :users  do
+    match 'users/sign_out', :to => 'devise/sessions#destroy'
+  end
+  
+   match  "/users/:personal_url", :to => "users#show",  :as =>  :show_user
+   match  "/users/", :to => "users#index",  :as =>  :users_path
+      #friends
+  resources :user_friends
+  match  "users/:personal_url/friends", :to => "users#friend", :as => :create_user_friends
+  match  "users/:personal_url/waiting_friends/:id/update", :to => "users#ufriend", :as => :update_user_friends
+  match  "users/:personal_url/waiting_friends/:id", :to => "users#sufriend", :as => :show_user_friends
+  match  "users/:personal_url/waiting_friends", :to => "users#waiting_friends", :as => :user_waiting_friends
+  
+  
+  #manejo de networks
+  
+  get "networks_users/create"
+
+  get "networks_users/new"
+  
+  match "networks_users/index", :to => "networks#register", :at => :networks_user
+
+  resources :network_templates
+
+  resources :networks
+  match '/' => 'networks#show', :constraints => { :subdomain => /.+/ }
+  
+  
+  #manejo de usuarios en las networks
+  resources :networks_users do 
+   collection do
+    post :create_data
+   end
+  end
+  
+  # manejo de la landing page
+  
+  get "home/index"
+
+  get "home/contact"
+
+  get "home/terms"
+
+  get "home/conditions"
+
+  get "home/team"
+
+  get "home/develop"
+
+  get "home/blog"
+
+  get "home/news"
+ 
+  root :to => 'home#index'
+
+  #comentarios
+  match "/home/add_new_comment" => "home#add_new_comment", :as => "add_new_comment", :via => [:post]
+
+  # The priority is based upon order of creation:
+  # first created -> highest priority.
+
+  # Sample of regular route:
+  #   match 'products/:id' => 'catalog#view'
+  # Keep in mind you can assign values other than :controller and :action
+
+  # Sample of named route:
+  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
+  # This route can be invoked with purchase_url(:id => product.id)
+
+  # Sample resource route (maps HTTP verbs to controller actions automatically):
+  #   resources :products
+
+  # Sample resource route with options:
+  #   resources :products do
+  #     member do
+  #       get 'short'
+  #       post 'toggle'
+  #     end
+  #
+  #     collection do
+  #       get 'sold'
+  #     end
+  #   end
+
+  # Sample resource route with sub-resources:
+  #   resources :products do
+  #     resources :comments, :sales
+  #     resource :seller
+  #   end
+
+  # Sample resource route with more complex sub-resources
+  #   resources :products do
+  #     resources :comments
+  #     resources :sales do
+  #       get 'recent', :on => :collection
+  #     end
+  #   end
+
+  # Sample resource route within a namespace:
+  #   namespace :admin do
+  #     # Directs /admin/products/* to Admin::ProductsController
+  #     # (app/controllers/admin/products_controller.rb)
+  #     resources :products
+  #   end
+
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  # root :to => 'welcome#index'
+
+  # See how all your routes lay out with "rake routes"
+
+  # This is a legacy wild controller route that's not recommended for RESTful applications.
+  # Note: This route will make all actions in every controller accessible via GET requests.
+  # match ':controller(/:action(/:id))(.:format)'
+end
