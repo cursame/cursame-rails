3e1a076ee2276c92395c657da5a38b79f7c23750
leonrangel <7@leonrangel.me>
2013-01-10 14:36:04 -0600

    me pide hacer commit
    no he movido nada

--- db/schema.rb
+++ db/schema.rb
@@ -55,9 +55,9 @@ ActiveRecord::Schema.define(:version => 20130109200119) do
     t.datetime "publish_date"
     t.datetime "end_date"
     t.integer  "porcent_of_evaluation"
+    t.integer  "course_id"
     t.datetime "created_at",            :null => false
     t.datetime "updated_at",            :null => false
-    t.integer  "course_id"
   end
 
   create_table "deliveries_courses", :id => false, :force => true do |t|
