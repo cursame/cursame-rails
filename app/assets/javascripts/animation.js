a9304d138833e84a6a8d18dc34c50f7ef0bf01e6
Rodrigo Ayala <azul.rejected@gmail.com>
2013-01-09 19:22:04 -0600

    Mejoras en cursos, DropDown y charla de SCSS

--- app/assets/javascripts/animation.js
+++ app/assets/javascripts/animation.js
@@ -113,4 +113,56 @@ $(document).ready(function() {
         $( "#tabs-courses" ).tabs();
         $( "#tabs-friends" ).tabs();
     });
+
+
+
+
+
+    function DropDown(el) {
+        this.dd = el;
+        this.opts = this.dd.find('ul.dropdown > li');
+        this.val = [];
+        this.index = [];
+        this.initEvents();
+    }
+    DropDown.prototype = {
+        initEvents : function() {
+            var obj = this;
+
+            obj.dd.on('click', function(event){
+                $(this).toggleClass('active');
+                event.stopPropagation();
+            });
+
+            obj.opts.children('label').on('click',function(event){
+                var opt = $(this).parent(),
+                    chbox = opt.children('input'),
+                    val = chbox.val(),
+                    idx = opt.index();
+                console.log(val);
+                //console.log($(opt).attr('name'));
+
+
+                //($.inArray(val, obj.val) !== -1) ? obj.val.splice( $.inArray(val, obj.val), 1 ) : obj.val.push( val );
+                //($.inArray(idx, obj.index) !== -1) ? obj.index.splice( $.inArray(idx, obj.index), 1 ) : obj.index.push( idx );
+
+
+            });
+        },
+        getValue : function() {
+            return this.val;
+        },
+        getIndex : function() {
+            return this.index;
+        }
+    }
+
+
+    var dd = new DropDown( $('#CourseDropDown') );
+
+    $(document).click(function() {
+        // all dropdowns
+        $('.wrapper-dropdown-4').removeClass('active');
+    });
+
 })
