a2392bf63043841139970e11115bd431a9fbb349
Rodrigo Ayala <azul.rejected@gmail.com>
2013-01-17 18:51:12 -0600

    Slider UI Funcionando

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 # It's strongly recommended to check this file into your version control system.
 
 ActiveRecord::Schema.define(:version => 20130116195213) do
-  
+
   create_table "answers", :force => true do |t|
     t.integer  "question_id"
     t.string   "content"
