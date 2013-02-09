2d09f7bb0dee443ccab8d0fe371bcb548815e8ba
Rodrigo Ayala <azul.rejected@gmail.com>
2013-02-08 18:49:07 -0600

    Animación dinámica para botones del coverphoto

--- app/assets/javascripts/aplication.animation.js
+++ app/assets/javascripts/aplication.animation.js
@@ -1,71 +1,108 @@
-//var checkBox = null;
+//=================================================================
+// When I wrote this, only God and I understood what I was doing
+// Now, God only knows - RodrigoAyala.me
+//=================================================================
+
 $(document).ready(function() {
 
     var time = 500;
     var timeSlide = time-100;
 
     /*
+     *======================================================================
+     *==================== Animation - Cover Photo Menu ====================
+     *======================================================================
      *
-     *=================================================================
-     *==================== Animation - Cover Photo ====================
-     *=================================================================
+     * El metodo $('#banner-btns > li').click() reconoce los elementos que contiene la caja "box-titles"
+     * y los relaciona con "box-titles-content", busca el tamaño de los contenedores, e inicializa la animación.
+     *
+     * Variables HTML obligatorias:
+     *    get-to:Id_box (Id de la caja)
+     *    move-sidebar-height:Pixels (Pixeles que se recorrera el sidebar)
+     *
+     * Ejemplo:
+     *
+     * <div id="box-titles">
+     *    <ul id="banner-btns" class="list-header">
+     *       <li class="calendar" get-to="#calendar-area" move-sidebar-height="36px" >Calendario</li>
+     *    </ul>
+     * </div>
+     * <div id="box-titles-content" class="box-titles-content-style">
+     *    <div id="calendar-area">
+     *        <img src="/assets/calendar/calendar.png" >
+     *    </div>
+     * </div>
      *
      */
 
-
-
-    //-----Titles Cover Photo -----
-    var control_height = false;
-    var tmp_height = undefined;
-
-    var padding_value = undefined;
-
-
-
-    $('#calendar-top-btn').click(function() {
-        if (control_height){
-            tmp_height = 140;
-            padding_value = '0px 0px 0px 0px';
-            control_height=false;
+    var defaultBannerHeight = 140;
+    var getElementId = undefined;
+    var tmpGetElementId = undefined;
+    var isOpen = undefined;
+    $('#banner-btns > li').click(function() {
+        getElementId = $(this).attr('get-to');
+        if(tmpGetElementId==getElementId){
+            if(isOpen){
+                closeBannerAnimation(tmpGetElementId);
+                isOpen = false;
+            }else{
+                openBannerAnimation( getElementId, defaultBannerHeight + $(getElementId).height() , $(this).attr('move-sidebar-height') );
+                isOpen = true;
+            }
+        }else if(tmpGetElementId != undefined && tmpGetElementId!=getElementId){
+            if(isOpen && tmpGetElementId!=getElementId){
+                closeAndOpenBannerAnimation(tmpGetElementId, getElementId, defaultBannerHeight + $(getElementId).height() , $(this).attr('move-sidebar-height'));
+                isOpen = true;
+            }else if(isOpen){
+                closeBannerAnimation(tmpGetElementId);
+                openBannerAnimation( getElementId, defaultBannerHeight + $(getElementId).height() , $(this).attr('move-sidebar-height') );
+                isOpen = false;
+            }else{
+                openBannerAnimation( getElementId, defaultBannerHeight + $(getElementId).height() , $(this).attr('move-sidebar-height') );
+                isOpen = true;
+            }
         }else{
-            tmp_height = 440;
-            padding_value = '36px 0px 0px 0px';
-            control_height=true;
+            openBannerAnimation( getElementId, defaultBannerHeight + $(getElementId).height() , $(this).attr('move-sidebar-height') );
+            isOpen = true;
         }
-
-        $('#banner-profile').animate({
-            height: tmp_height+'px'
-        }, time);
-        $('.box-3').animate({
-            padding: padding_value
-        }, time);
+        tmpGetElementId = getElementId;
     });
-
-
-    //-----STATS -----
-
-    //-----Titles Cover Photo -----
-    var control_height_2 = false;
-    var tmp_height_2 = 140;
-
-    $('#stats').click(function() {
-        if (control_height_2){
-            tmp_height_2 = 140;
-            padding_value = '0px 0px 0px 0px';
-            control_height_2=false;
-
-        }else{
-            tmp_height_2 = 240;
-            padding_value = '106px 0px 0px 0px';
-            control_height_2=true;
-        }
+    function closeBannerAnimation(elementHide){
         $('#banner-profile').animate({
-            height: tmp_height_2+'px'
+            height: defaultBannerHeight+'px'
+        }, time, function() {
+            $(elementHide).hide();
+        });
+        $('#side-bar').animate({
+            padding: '0px 0px 0px 0px'
         }, time);
-        $('.box-3').animate({
-            padding: padding_value
+    }
+    function openBannerAnimation( elementHide, bannerProfileHeight , slideBarPaddValue ){
+        $(elementHide).show();
+        $('#banner-profile').animate({
+            height: bannerProfileHeight+'px'
         }, time);
-    });
+        $('#side-bar').animate({
+            padding: slideBarPaddValue + ' 0px 0px 0px'
+        }, time);
+    }
+    function closeAndOpenBannerAnimation( elementHide, elementShow, bannerProfileHeight , slideBarPaddValue ){
+        $('#banner-profile').animate({
+            height: defaultBannerHeight+'px'
+        }, time, function() {
+            $(elementHide).hide();
+            $(elementShow).show();
+            $('#banner-profile').animate({
+                height: bannerProfileHeight+'px'
+            }, time);
+            $('#side-bar').animate({
+                padding: slideBarPaddValue + ' 0px 0px 0px'
+            }, time);
+        });
+        $('#side-bar').animate({
+            padding: '0px 0px 0px 0px'
+        }, time);
+    }
 
 
     /*
@@ -73,14 +110,10 @@ $(document).ready(function() {
      *==================== Animation - Menu Post ====================
      *===============================================================
      */
-
     var prePostHeight = $('#profile-form-options').height();
     var prePostMargin = $('#profile-form-options').css('margin');
-
-
     $('#profile-form-options > a').click(function() {
         var hrefClean = $(this).attr('href');
-        console.log($( hrefClean ).height());
         $(this).parent().animate({  // Se va por $('#profile-form-options')
             opacity: 0.0,
             height: '0',
@@ -92,13 +125,13 @@ $(document).ready(function() {
             height: $( hrefClean ).height()
         }, time);
         $( hrefClean ).css('display','block');
-        $(hrefClean ).animate({
+        $( hrefClean ).animate({
             opacity: 1
         }, time);
         return false;
     });
     //Cancel btns
-    $('#post-forms>div').each(function( index, value ) {
+    $('#post-forms > div').each(function( index, value ) {
         $(value).find('#cancel-post-form').click(function() {
             resetForm($(value).find('form'));
             $('#post-forms').animate({
@@ -118,7 +151,14 @@ $(document).ready(function() {
 
         });
     });
-    //hoverBtn(idBtn, border, colorBg)
+
+
+
+    /*
+     *===============================================================================
+     *==================== Animation - Items Menu Post (Circles) ====================
+     *===============================================================================
+     */
     $('.message-form-btn, .delivery-form-btn, .discussion-form-btn, .survey-form-btn').hover(function() {
         switch ($(this).attr('class')) {
             case "message-form-btn":
@@ -138,11 +178,51 @@ $(document).ready(function() {
         hoverOutBtn( '.'+$(this).attr('class') );
     });
 
+    var borderShadow;
+    var borderMargin;
+    var iconShadow;
+    var iconMargin;
+    var iconBackgroundColor;
+    var iconBorderColor;
+    function hoverBtn( obj , borderColor, backgroundColor){
+        borderShadow = $(obj +' .border').css('box-shadow');
+        borderMargin = $(obj +' .border').css('margin');
+        iconShadow = $(obj +" .icon").css('box-shadow');
+        iconMargin = $(obj +" .icon").css('margin');
+        iconBorderColor = $(obj +" .icon").css('border-color');
+        iconBackgroundColor = $(obj +" .icon").css('background-color');
+
+        $(obj +' .border').animate({
+            boxShadow: '0px 0px 0px #FFF',
+            margin:     '3px 0px 3px 18px'
+        }, 200);
+
+        $(obj+" .icon").animate({
+            boxShadow: '0px 2px 5px #555',
+            margin:     '-2px 0px 0px -2px',
+            backgroundColor: backgroundColor,
+            borderColor:borderColor
+        }, 200);
+    }
+    function hoverOutBtn(obj){
+        $(obj+' .border').animate({
+            boxShadow: borderShadow ,
+            margin:     borderMargin
+        }, 200);
+        $(obj+" .icon").animate({
+            boxShadow: iconShadow,
+            margin:     iconMargin,
+            backgroundColor: iconBackgroundColor ,
+            borderColor: iconBorderColor
+        }, 200);
+    }
+
     /*
-     *=====================================================================================
-     *==================== Courses Animation (Create) - slide & resize ====================
-     *=====================================================================================
+     *===========================================================================================
+     *==================== Courses Animation (Create) - tabs, slide & resize ====================
+     *===========================================================================================
      */
+
     var tmp_height_new_course = $('#form-new-course').height();
     var tmp_height_new_course_box = null;
     $('#new-course-btn').click(function(){
@@ -150,14 +230,11 @@ $(document).ready(function() {
         $('#box-course').animate({
             height: tmp_height_new_course+'px'
         }, 400);
-
         moveOutSlide();
-
         var nestedNewCourseSlider = new NestedSlider(false);
         nestedNewCourseSlider.initSlider ( $('#slider-course-1'), $('#course_survey_param_evaluation') );
         nestedNewCourseSlider.initSlider ( $('#slider-course-2'), $('#course_delivery_param_evaluation') );
     });
-
     $('#cancel-course-form, #return-course') .click(function(){
         $('#box-course').animate({
             height: tmp_height_new_course_box+'px'
@@ -166,90 +243,35 @@ $(document).ready(function() {
             //$(this).css('height','');
         });
         moveInSlide();
-
-
         resetForm($('#new_course'));
     });
-
     function moveOutSlide(){
-        $('#tabs-courses').hide(
-            "slide", { direction: "left" }, 1000
-        );
-        $('#form-new-course').show(
-            "slide",
-            {
-                direction: "right"
-            },1000);
+        $('#tabs-courses').hide("slide", {
+            direction: "left"
+        }, 1000);
+        $('#form-new-course').show("slide",{
+            direction: "right"
+        },1000);
     }
-
     function moveOutSlide(){
-        $('#tabs-courses').hide(
-            "slide", { direction: "left" }, timeSlide
-        );
-        $('#form-new-course').show(
-            "slide",
-            {
-                direction: "right"
-            },timeSlide);
+        $('#tabs-courses').hide("slide", {
+           direction: "left"
+        }, timeSlide);
+        $('#form-new-course').show("slide",{
+            direction: "right"
+        },timeSlide);
     }
 
     function moveInSlide(){
-        $('#form-new-course').hide(
-            "slide",
-            {
-                direction: "right"
-            },timeSlide);
-        $('#tabs-courses').show(
-            "slide", { direction: "left" }, timeSlide
-        );
+        $('#form-new-course').hide("slide",{
+            direction: "right"
+        },timeSlide);
+        $('#tabs-courses').show("slide", {
+            direction: "left"
+        }, timeSlide);
     }
-
-
     $( "#tabs-courses" ).tabs();
     $( "#tabs-friends" ).tabs();
 
 
 });
-
-
-
-//Menu post buttons
-var borderShadow;
-var borderMargin;
-var iconShadow;
-var iconMargin;
-var iconBackgroundColor;
-var iconBorderColor;
-
-function hoverBtn( obj , borderColor, backgroundColor){
-    borderShadow = $(obj +' .border').css('box-shadow');
-    borderMargin = $(obj +' .border').css('margin');
-    iconShadow = $(obj +" .icon").css('box-shadow');
-    iconMargin = $(obj +" .icon").css('margin');
-    iconBorderColor = $(obj +" .icon").css('border-color');
-    iconBackgroundColor = $(obj +" .icon").css('background-color');
-
-    $(obj +' .border').animate({
-        boxShadow: '0px 0px 0px #FFF',
-        margin:     '3px 0px 3px 18px'
-    }, 200);
-
-    $(obj+" .icon").animate({
-        boxShadow: '0px 2px 5px #555',
-        margin:     '-2px 0px 0px -2px',
-        backgroundColor: backgroundColor,
-        borderColor:borderColor
-    }, 200);
-}
-function hoverOutBtn(obj){
-    $(obj+' .border').animate({
-        boxShadow: borderShadow ,
-        margin:     borderMargin
-    }, 200);
-    $(obj+" .icon").animate({
-        boxShadow: iconShadow,
-        margin:     iconMargin,
-        backgroundColor: iconBackgroundColor ,
-        borderColor: iconBorderColor
-    }, 200);
-}
