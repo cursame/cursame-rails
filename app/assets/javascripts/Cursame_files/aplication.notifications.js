89569349f25e8e8879264ce80978e139ccef90fa
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2013-02-11 19:08:03 -0600

    subiendo faltantes de create roles

---
+++ app/assets/javascripts/Cursame_files/aplication.notifications.js
@@ -0,0 +1,13 @@
+$(function(){
+	$('#notifications_count').on('click',function(e){
+		e.preventDefault();
+		$('#box-notifications').toggle();
+	});
+});
+     /*
+$(document).click(function(e) {
+    e.preventDefault();
+    $('#box-notifications').toggle();
+});*/
+
+;
