9337f895e2d7fef8a17d7606f895f9e64380cdeb
Leon Rangel <7@leonrangel.me>
2013-02-11 19:08:54 -0600

    iconos en menu de creación

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(:version => 20130212005424) do
+=======
+ActiveRecord::Schema.define(:version => 20130208210650) do
+>>>>>>> iconos en menu de creación
 
   create_table "answers", :force => true do |t|
     t.integer  "question_id"
