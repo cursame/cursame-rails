a006386ca7ef5d8d5768edad0f4ad04b5ab5b822
jose armando gonzalez lopez <iam@armando.mx>
2013-02-12 14:32:00 -0600

    se corrigio el error de las creaciones duplicadas, ademas la creacion  y el envio de notificaciones para cursos y deliveries :)

--- app/models/course.rb
+++ app/models/course.rb
@@ -21,17 +21,18 @@ class Course < ActiveRecord::Base
   #comentarios para las redes
   acts_as_commentable 
 
-=begin   
+  
   after_create do
     if self.public_status == 'public'
-      #self.network.networks_users.each do |u|
-      User.all.each do |u|
-        #Notification.create :user => u, :notificator => self, :kind => 'new_public_course_on_network'
-        Wall.create :user => u, :publication => self
+      self.members_in_courses.each do |u|
+        user = User.find_by_id(u.user_id)
+        if u.owner != true
+          Notification.create :user => user, :notificator => self, :kind => 'new_public_course_on_network'          
+        end
+        Wall.create :user => user, :publication => self
       end
     end  
   end
-=end
 
   def self.search(search)
     if search
@@ -40,6 +41,7 @@ class Course < ActiveRecord::Base
       find(:all, :order => :title)
     end
   end
+  
   ####es necesario sustituir el tipo de lectura de la relación manualmente #####
   ########## se crea el campo users en el modelo para poder generar la lectura correcta dentro de las relaciones ######
   def razon_users
@@ -55,7 +57,7 @@ class Course < ActiveRecord::Base
     users_invoque.each do |invoque|
       @ids.push(invoque.user_id)     
     end
-      @users = User.find_by_id(@ids)
+      @users = User.find(@ids)
   end
   
   
