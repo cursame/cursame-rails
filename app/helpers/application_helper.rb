5bd24691287753e09cce59794031cf3c6753eb43
Rodrigo Ayala <azul.rejected@gmail.com>
2013-02-14 18:33:40 -0600

    Modificacion del formulario de Surveys

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,6 @@ module ApplicationHelper
     fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
       render('/surveys/'+association.to_s.singularize + "_fields", :f => builder)
     end
-    link_to_function(name, "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")")
+    link_to_function(name, "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")", {:class=>'a-btn'})
   end
 end
