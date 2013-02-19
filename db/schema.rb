88c34bd3829a3cdd8210f4ddb0b3013f2d23f8df
Rodrigo Ayala <azul.rejected@gmail.com>
2013-02-19 15:07:51 -0600

    …

--- db/schema.rb
+++ db/schema.rb
@@ -15,18 +15,14 @@ ActiveRecord::Schema.define(:version => 20130218172833) do
 
   create_table "activities", :force => true do |t|
     t.string   "title"
-    t.string   "activitye_type"
-    t.integer  "activitye_id"
+    t.string   "activity_type"
+    t.integer  "activable"
+    t.string   "place"
     t.string   "address"
     t.float    "latitude"
     t.float    "longitude"
-    t.string   "ip_address"
-    t.string   "browser"
-    t.string   "version_browser"
-    t.string   "computer_plataform"
-    t.integer  "user_id"
-    t.datetime "created_at",         :null => false
-    t.datetime "updated_at",         :null => false
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
   end
 
   create_table "answers", :force => true do |t|
@@ -135,9 +131,9 @@ ActiveRecord::Schema.define(:version => 20130218172833) do
     t.datetime "publish_date"
     t.datetime "end_date"
     t.integer  "porcent_of_evaluation"
-    t.integer  "course_id"
     t.datetime "created_at",            :null => false
     t.datetime "updated_at",            :null => false
+    t.integer  "course_id"
     t.integer  "user_id"
     t.string   "state"
     t.integer  "network_id"
@@ -232,15 +228,6 @@ ActiveRecord::Schema.define(:version => 20130218172833) do
     t.datetime "updated_at", :null => false
   end
 
-  create_table "notificacions", :force => true do |t|
-    t.integer  "notificator_id"
-    t.string   "notificator_type"
-    t.integer  "user_id"
-    t.string   "kind"
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
-  end
-
   create_table "notifications", :force => true do |t|
     t.integer  "notificator_id"
     t.string   "notificator_type"
@@ -280,12 +267,6 @@ ActiveRecord::Schema.define(:version => 20130218172833) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.integer  "value"
-  end
-
-  create_table "recipes", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
   end
 
   create_table "response_to_the_evaluations", :force => true do |t|
