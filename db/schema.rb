0e23c7946fe6852d085c7df1d2252727201f42f7
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2013-02-20 18:44:29 -0600

    agregando user_id to assignments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130220184019) do
+ActiveRecord::Schema.define(:version => 20130221004321) do
 
   create_table "activities", :force => true do |t|
     t.string   "title"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(:version => 20130220184019) do
     t.datetime "created_at",        :null => false
     t.datetime "updated_at",        :null => false
     t.integer  "course_id"
+    t.integer  "user_id"
   end
 
   create_table "authentications", :force => true do |t|
