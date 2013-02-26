e08e6208918fe7e777003db1ea2123dde046f454
Emiliano Cabrera <jemiliano.cabrera@gmail.com>
2013-02-25 19:12:07 -0600

    Crea la tabla de califaciones para el usuario y para el teacher

--- app/models/members_in_course.rb
+++ app/models/members_in_course.rb
@@ -2,12 +2,11 @@ class MembersInCourse < ActiveRecord::Base
   belongs_to :course
   belongs_to :user
   
-  def assignment
-    
-    
+  def deliveries
+    #return Delivery.where()
   end
   
-  def evaluation
+  def evaluationSurveys
     course = Course.find(self.course_id)
     evaluationSurveys = 0.0
     course.surveys.each do |response|
@@ -21,7 +20,13 @@ class MembersInCourse < ActiveRecord::Base
       end
       evaluationSurveys += user_survey.result
     end
-    evaluationSurveys = evaluationSurveys/course.surveys.size 
+    evaluationSurveys = evaluationSurveys/course.surveys.size
+    return evaluationSurveys.to_i 
+  end
+  
+  def evaluationDeliverys
+    course = Course.find(self.course_id)
+    evaluationSurveys = evaluationSurveys 
     
     assignments = Assignment.where(:course_id => self.course_id, :user_id => self.user_id) 
     evaluationDeliverys = 0.0
@@ -29,6 +34,10 @@ class MembersInCourse < ActiveRecord::Base
     assignments.each do |response|
       evaluationDeliverys += response.accomplishment.to_f
     end
+    return evaluationDeliverys.to_i
+  end
+  
+  def evaluation
     
     survey_param_evaluation = (course.survey_param_evaluation.to_f)/100.0
     
@@ -39,4 +48,63 @@ class MembersInCourse < ActiveRecord::Base
     
     return evaluation
   end
+  
+  #
+  # table_member[0] => Tareas ordenadas de acerudo a la fecha de creacion en la base de datos
+  # table_member[1] => Calificaciones de cada tarea.
+  # table_member[2] => Calificacion total de las tareas.
+  # table_member[2][0..2] => Valor de las tareas, calificiacion en 100%, calificacion del total.
+  #
+  def doTableDeliveries
+    table_member = Array.new
+    course = Course.find(self.course_id)
+    deliveries = course.deliveries.sort{|x,y| x.created_at <=> y.created_at}
+    table_member[0] = deliveries
+    assignments_array = Array.new
+    for i in 0...deliveries.size
+      assignments_array[i] =  Assignment.find_by_delivery_id_and_user_id(deliveries[i].id,self.user_id)
+    end
+    table_member[1] = assignments_array
+    evaluation_total_array = Array.new
+    evaluation_total_array[0] = course.delivery_param_evaluation
+    evaluation_total_array[1] = self.evaluationDeliverys
+    delivery_param_evaluation = course.delivery_param_evaluation.to_f/100.0
+    table_member[2] = evaluation_total_array
+    return table_member
+  end
+  
+  
+  #
+  # table_member[0] => Lista de Surveys
+  # table_member[1] => Lista de User_Surveys
+  # table_member[2] => Calificacion total
+  # table_member[2][0..2] => Valor de los examenes, calificiacion en 100%, calificacion del total.
+  #
+  def doTableSurveys
+    table_member = Array.new
+    course = Course.find(self.course_id)
+    surveys = course.surveys.sort{ |x,y| x.created_at <=> y.created_at}
+    table_member[0] = surveys
+    user_survey_array = Array.new
+    for i in 0...surveys.size
+      user_survey_array[i] = UserSurvey.find_by_survey_id_and_user_id(surveys[i].id,self.user_id)
+    end
+    table_member[1] = user_survey_array
+    evaluation_total_array = Array.new
+    evaluation_total_array[0] = course.survey_param_evaluation
+    evaluation_total_array[1] = self.evaluationSurveys
+    survey_param_evaluation = course.survey_param_evaluation.to_f/100.0
+    evaluation_total_array[2] = evaluation_total_array[1] * survey_param_evaluation
+    evaluation_total_array[1] = self.evaluationSurveys/10.0
+    table_member[2] = evaluation_total_array
+    return table_member
+  end
+  
+  def doTableCourse
+    table_course = Array.new
+    table_course[0] = doTableDeliveries
+    table_course[1] = doTableSurveys
+    return table_course
+  end
+  
 end
