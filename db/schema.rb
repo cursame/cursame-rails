9013b12b9f93431c5a321679a53612893c1f7557
Rodrigo Ayala <azul.rejected@gmail.com>
2013-02-28 16:32:59 -0600

    ...

--- db/schema.rb
+++ db/schema.rb
@@ -15,18 +15,14 @@ ActiveRecord::Schema.define(:version => 20130227231205) do
 
   create_table "activities", :force => true do |t|
     t.string   "title"
-    t.string   "activitye_type"
-    t.integer  "activitye_id"
+    t.string   "activity_type"
+    t.integer  "activable"
+    t.string   "place"
     t.string   "address"
     t.float    "latitude"
     t.float    "longitude"
-    t.string   "ip_address"
-    t.string   "browser"
-    t.string   "version_browser"
-    t.string   "computer_plataform"
-    t.integer  "user_id"
-    t.datetime "created_at",         :null => false
-    t.datetime "updated_at",         :null => false
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
     t.integer  "network_id"
   end
 
