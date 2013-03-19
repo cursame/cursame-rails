de5d0cfec66a007a5c2d0b5d94cf4a97769ad7f0
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2013-03-18 20:04:33 -0600

    subiendo listo para merge y crear super admin

--- db/migrate/20130227183957_create_cursame_super_admin.rb
+++ db/migrate/20130227183957_create_cursame_super_admin.rb
@@ -1,10 +1,10 @@
 class CreateCursameSuperAdmin < ActiveRecord::Migration
   def up
-=begin
+
     User.create(email:"info@cursa.me", first_name: "super", last_name: "admin", password: "cursameautonomo201330lb", personal_url: "superadmin")
          a = User.last
          Permissioning.create( user_id: a.id, role_id: "4")
-=end
+
   end
 
   def down
