b7ad3fa3952a109a4eef78e751fc1606cba28331
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2013-03-18 20:44:30 -0600

    subiendo cambios en migrate2

--- db/migrate/20130311163533_change_rub_calification_to_float.rb
+++ db/migrate/20130311163533_change_rub_calification_to_float.rb
@@ -1,6 +1,6 @@
 class ChangeRubCalificationToFloat < ActiveRecord::Migration
   def up
-    remove_column :assignments, :rub_calification
+  #  remove_column :assignments, :rub_calification
     add_column :assignments, :rub_calification, :float
   end
 
