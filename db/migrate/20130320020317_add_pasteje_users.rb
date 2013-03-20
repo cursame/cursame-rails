8d01629cd3e0b006aee3ee5c31c7330d2a498a29
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2013-03-19 20:34:42 -0600

    subiendo cambios para migrate recibir

--- db/migrate/20130320020317_add_pasteje_users.rb
+++ db/migrate/20130320020317_add_pasteje_users.rb
@@ -5,12 +5,20 @@ class AddPastejeUsers < ActiveRecord::Migration
           User.create(:first_name => "", :last_name => "", :email => "", :password => "")
             @user = User.last
             @course = Course.last
-          MembersInCourse.create(:user_id => @user.id, :course_id => @course.id, :accepted => true, :owner => false, :network_id => 1 )
+            puts "creando estudiantes primer curso"
+            Permissioning.create(:user_id => @user.id, :role_id => "2", :network_id => "1"  )
+            puts "creando maestros primer curso"
+            Permissioning.create(:user_id => @user.id, :role_id => "3", :network_id => "1"  )
+            
+          MembersInCourse.create(:user_id => @user.id, :course_id => @course.id, :accepted => true, :owner => false, :network_id => "1" )
       puts "se crea el segundo curso"
           Course.create(:title => "Sexto B", :init_date => "01/08/2012", :finish_date => "31/07/2013", :public_status => "private")
           User.create(:first_name => "", :last_name => "", :email => "", :password => "")
-          MembersInCourse.create(:user_id => @user.id, :course_id => @course.id, :accepted => true, :owner => false, :network_id => 1 )
-          
+          MembersInCourse.create(:user_id => @user.id, :course_id => @course.id, :accepted => true, :owner => false, :network_id => "1" )
+               puts "creando estudiantes segundo curso"
+               Permissioning.create(:user_id => @user.id, :role_id => "2", :network_id => "1"  )
+               puts "creando maestros segundo curso"
+               Permissioning.create(:user_id => @user.id, :role_id => "3", :network_id => "1"  )
   end
 
   def down
