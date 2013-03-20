317e5f13825836eb2623988cc9845b01ea05598a
Luis Alfredo Lorenzo <wichobabas@gmail.com>
2013-03-20 04:43:12 -0600

    migracion correccion de curso usuarios

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130316011330) do
+ActiveRecord::Schema.define(:version => 20130320020317) do
 
   create_table "activities", :force => true do |t|
     t.string   "title"
@@ -107,6 +107,7 @@ ActiveRecord::Schema.define(:version => 20130316011330) do
   add_index "comments", ["user_id"], :name => "index_comments_on_user_id"
 
   create_table "compart_assets", :force => true do |t|
+    t.string   "asset"
     t.integer  "asset_id"
     t.integer  "delivery_id"
     t.integer  "assignment_id"
@@ -244,15 +245,6 @@ ActiveRecord::Schema.define(:version => 20130316011330) do
     t.datetime "updated_at", :null => false
   end
 
-  create_table "notificacions", :force => true do |t|
-    t.integer  "notificator_id"
-    t.string   "notificator_type"
-    t.integer  "user_id"
-    t.string   "kind"
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
-  end
-
   create_table "notifications", :force => true do |t|
     t.integer  "notificator_id"
     t.string   "notificator_type"
@@ -299,12 +291,6 @@ ActiveRecord::Schema.define(:version => 20130316011330) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.integer  "value"
-  end
-
-  create_table "recipes", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
   end
 
   create_table "response_to_the_evaluations", :force => true do |t|
