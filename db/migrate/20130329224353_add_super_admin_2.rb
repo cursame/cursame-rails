a9c0d614c22e7f72a72aabe24e145d814691d96d
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2013-03-29 16:57:20 -0600

    subiendo cambios en migrate alternativo

--- db/migrate/20130329224353_add_super_admin_2.rb
+++ db/migrate/20130329224353_add_super_admin_2.rb
@@ -1,5 +1,10 @@
 class AddSuperAdmin2 < ActiveRecord::Migration
   def up
+      Role.create( title: "admin", description: "administrador de una red" ) 
+      Role.create( title: "student", description: "estudiante de una red" ) 
+      Role.create( title: "teacher", description: "profesor de una red" ) 
+      Role.create( title: "superadmin", description: "superadmin del sistema" ) 
+      Role.create( title: "supervisor", description: "admin sin capacidad de modificacion en cosas importantes en la red" )
 
      User.create(email:"info@cursa.me", first_name: "super", last_name: "admin", password: "cursameautonomo201330lb", personal_url: "superadmin")
           a = User.last
