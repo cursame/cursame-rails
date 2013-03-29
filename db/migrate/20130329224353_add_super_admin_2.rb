7c40379e2a7c43eab596578fe23afd56077825ac
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2013-03-29 16:45:55 -0600

    subiendo creación de super admin 2

---
+++ db/migrate/20130329224353_add_super_admin_2.rb
@@ -0,0 +1,12 @@
+class AddSuperAdmin2 < ActiveRecord::Migration
+  def up
+
+     User.create(email:"info@cursa.me", first_name: "super", last_name: "admin", password: "cursameautonomo201330lb", personal_url: "superadmin")
+          a = User.last
+          Permissioning.create( user_id: a.id, role_id: "4")
+
+   end
+
+   def down
+   end
+end
