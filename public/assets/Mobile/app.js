0c52bf2685395cd7f969de7370000e96888681f6
waldix <waldix86@gmail.com>
2013-04-02 01:04:44 -0600

    Se sube Mobile Compresa.

--- public/assets/Mobile/app.js
+++ public/assets/Mobile/app.js
@@ -70560,72 +70560,85 @@ Ext.define('Cursame.model.Publication', {
     extend: 'Ext.data.Model',
 
     config: {
-        fields: [{
+        fields: [
+            {
                 name: "id",
                 type: "int"
-            }, {
+            },
+            {
                 name: "publication_type",
                 type: "string",
                 convert: function (v) {
                     return v ? v.toLowerCase() : 'comment';
                 }
-            }, {
+            },
+            {
                 name: "publication_id",
                 type: "int"
-            }, {
+            },
+            {
                 name: "user_id",
                 type: "int"
-            }, {
+            },
+            {
                 name: "publication",
                 type: 'object'
-            }, {
+            },
+            {
                 name: "network",
                 type: 'object'
-            }, {
+            },
+            {
                 name: 'avatar',
                 type: 'string',
                 mapping: 'publication',
                 convert: function (v, r) {
-                    return Cursame.ASSETSURL+'resources/images/curso.jpg';
+                    return Cursame.ASSETSURL + 'resources/images/curso.jpg';
                 }
-            },{
-                name:'course',
+            },
+            {
+                name: 'course',
                 type: 'object'
-            },{
-                name:'user',
+            },
+            {
+                name: 'user',
                 type: 'object'
-            },{
-                name:'content',
-                mapping:'publication',
-                type:'string',
-                 convert: function (v, r) {
+            },
+            {
+                name: 'content',
+                mapping: 'publication',
+                type: 'string',
+                convert: function (v, r) {
                     var content = '',
                         course = r.get('course'),
                         user = r.get('user'),
                         publication = r.get('publication');
-                    switch(r.get('publication_type')){
-                        case 'discussion':
-                            content = publication.title+' </br> ';
-                            content += publication.description;
-                        break;
-                        case 'delivery':
-                            content = publication.title+' </br> ';
-                            content += publication.description;
-                        break;
-                        case 'comment':
-                            content = publication.comment_html;
-                        break;
-                        case 'course':
-                            content = publication.title+' </br> ';
-                            content += publication.silabus;
-                        break;
-                        case 'survey':
-                            content = publication.name;
-                        break;
+                    if (publication) {
+                        switch (r.get('publication_type')) {
+                            case 'discussion':
+                                content = publication.title + ' </br> ';
+                                content += publication.description;
+                                break;
+                            case 'delivery':
+                                content = publication.title + ' </br> ';
+                                content += publication.description;
+                                break;
+                            case 'comment':
+                                content = publication.comment_html;
+                                break;
+                            case 'course':
+                                content = publication.title + ' </br> ';
+                                content += publication.silabus;
+                                break;
+                            case 'survey':
+                                content = publication.name;
+                                break;
+                        }
                     }
                     return content;
                 }
-            },{
+            },
+            {
                 name: 'title',
                 type: 'string',
                 mapping: 'publication',
@@ -70633,28 +70646,29 @@ Ext.define('Cursame.model.Publication', {
                     var title = '',
                         course = r.get('course'),
                         user = r.get('user');
-                    switch(r.get('publication_type')){
+                    switch (r.get('publication_type')) {
                         case 'discussion':
                             title = 'Discusión nueva ';
-                            title += course ? 'en el curso de <b>'+ course.title+'</b>': '<b>en la red' +'</b>';
-                        break;
+                            title += course ? 'en el curso de <b>' + course.title + '</b>' : '<b>en la red' + '</b>';
+                            break;
                         case 'delivery':
-                            title = 'Se ha creado una tarea en el curso <b>'+ course.title+'</b>';
-                        break;
+                            title = 'Se ha creado una tarea en el curso <b>' + course.title + '</b>';
+                            break;
                         case 'comment':
                             title = 'Comentario  ';
-                            title += course ? 'en el curso de <b>'+ course.title+'</b>': '<b>en la red' +'</b>';
-                        break;
+                            title += course ? 'en el curso de <b>' + course.title + '</b>' : '<b>en la red' + '</b>';
+                            break;
                         case 'course':
-                            title = 'Curso nuevo en la red <b>'+course.title+'</b>';
-                        break;
+                            title = 'Curso nuevo en la red <b>' + course.title + '</b>';
+                            break;
                         case 'survey':
-                            title = 'Se ha creado un cuestionario en el curso <b>'+ course.title+'</b>';
-                        break;
+                            title = 'Se ha creado un cuestionario en el curso <b>' + course.title + '</b>';
+                            break;
                     }
                     return title;
                 }
-            }, {
+            },
+            {
                 name: 'created',
                 type: 'date',
                 mapping: 'created_at',
@@ -70683,71 +70697,73 @@ Ext.define('Cursame.model.Publication', {
                         return '';
                     }
                 }
-            },{
-            name: 'num_comments',
-            type: 'int',
-            mapping: 'comments',
-            convert: function (comments, r){
-                return comments?comments.length:0;
-            }
-        },{
-            name: 'likes',
-            type: 'object',
-            mapping: 'publication',
-            convert: function (publication, r){
-                var likes = 0;
-                if(publication){
-                    if(publication.votes){
-                        likes =  publication.votes.length
-                    } else {
-                        likes = publication;
+            },
+            {
+                name: 'num_comments',
+                type: 'int',
+                mapping: 'comments',
+                convert: function (comments, r) {
+                    return comments ? comments.length : 0;
+                }
+            },
+            {
+                name: 'likes',
+                type: 'object',
+                mapping: 'publication',
+                convert: function (publication, r) {
+                    var likes = 0;
+                    if (publication) {
+                        if (publication.votes) {
+                            likes = publication.votes.length
+                        } else {
+                            likes = publication;
+                        }
                     }
+                    return likes;
                 }
-                return likes;
-            }
-        },
-        {
-            name:'headerAvatar',
-            type:'string',
-            convert:function(headerAvatar, r){
-                var url = Cursame.URL+'/assets/course-avatarx-0a909a23b940f3f1701b2e6065c29fe6.png';
-                if(headerAvatar){
-                    url = Cursame.URL+headerAvatar
+            },
+            {
+                name: 'headerAvatar',
+                type: 'string',
+                convert: function (headerAvatar, r) {
+                    var url = Cursame.URL + '/assets/course-avatarx-0a909a23b940f3f1701b2e6065c29fe6.png';
+                    if (headerAvatar) {
+                        url = Cursame.URL + headerAvatar
+                    }
+                    return url;
                 }
-                return url;
-            }
-        },
-        {
-            name:'headerTitle',
-            type:'string'
-        },
-        {
-            name:'headerPublicStatus',
-            type:'string'
-        },
-        {
-            name:'emptyStore',
-            type:'string'
-        },
-            {
-                name:'headerInitDate',
-                type:'string'
             },
             {
-                name:'headerFinishDate',
-                type:'string'
+                name: 'headerTitle',
+                type: 'string'
             },
             {
-                name:'headerSilabus',
-                type:'string'
+                name: 'headerPublicStatus',
+                type: 'string'
             },
             {
-                name:'headerId',
-                type:'int'
+                name: 'emptyStore',
+                type: 'string'
             },
             {
-                name:'showHeader',
-                type:'string'
+                name: 'headerInitDate',
+                type: 'string'
+            },
+            {
+                name: 'headerFinishDate',
+                type: 'string'
+            },
+            {
+                name: 'headerSilabus',
+                type: 'string'
+            },
+            {
+                name: 'headerId',
+                type: 'int'
+            },
+            {
+                name: 'showHeader',
+                type: 'string'
             }
         ],
         proxy: {
