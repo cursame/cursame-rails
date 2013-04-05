a0dc1b0654bbac546f0b363e49f0e08bbb4b76fa
jose armando gonzalez lopez <iam@armando.mx>
2013-04-05 05:13:07 -0600

    se cambio el fitro del listado del muro, por current_network.walls

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,16 +12,16 @@ User.create(:email => "info+alejo@cursa.me", :first_name => 'Alejo', :last_name 
 User.create(:email => "info+juan_roman@cursa.me", :first_name => 'Juan ', :last_name => 'Roman Escamilla', :personal_url => 'JuanRomanEscamilla', :password => "juanre1"  )
     b = User.last
      Permissioning.create( user_id: b.id, role_id: "1", :network_id => '2')
-User.create(:email => "isc_carlosdv@hotmail.com", :first_name => 'Carlos ', :last_name => 'Dominguez Valente', :personal_url => 'CarlosDominguezValente', :password => "carlosdv1" )
+User.create(:email => "info+isc_carlosdv@cursa.me", :first_name => 'Carlos ', :last_name => 'Dominguez Valente', :personal_url => 'CarlosDominguezValente', :password => "carlosdv1" )
     c = User.last
      Permissioning.create( user_id: c.id, role_id: "1", :network_id => '2')
-User.create(:email => "umendoza@iusa.com.mx", :first_name => 'Ubaldo ', :last_name => 'Mendoza', :personal_url => 'UbaldoMendoza', :password => "ubaldom1" )
+User.create(:email => "info+umendoza@cursa.me", :first_name => 'Ubaldo ', :last_name => 'Mendoza', :personal_url => 'UbaldoMendoza', :password => "ubaldom1" )
     d = User.last
     Permissioning.create( user_id: d.id, role_id: "1", :network_id => '2')
-User.create(:email => "madayriv@gmail.com", :first_name => 'Sara ', :last_name => 'Maday', :personal_url => 'SaraMaday', :password => "saram1" )
+User.create(:email => "info+madayriv@cursa.me", :first_name => 'Sara ', :last_name => 'Maday', :personal_url => 'SaraMaday', :password => "saram1" )
     e = User.last
     Permissioning.create( user_id: e.id, role_id: "1", :network_id => '2')
-User.create(:email => "curosas.lu@gmail.com", :first_name => 'Lucia ', :last_name => 'Cruz Rojas', :personal_url => 'LuciaCruzRojas', :password => "liciacr1" )
+User.create(:email => "info+curosas.lu@cursa.me", :first_name => 'Lucia ', :last_name => 'Cruz Rojas', :personal_url => 'LuciaCruzRojas', :password => "liciacr1" )
     f = User.last
     Permissioning.create( user_id: f.id, role_id: "1", :network_id => '2')
 
