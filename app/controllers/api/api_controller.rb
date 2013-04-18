ca4d48ba0c13bc3f7cbf2b0faed68c0fee2587e6
waldix <waldix86@gmail.com>
2013-04-17 20:57:26 -0500

    Se sube curso de notificaciones.

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -71,10 +71,11 @@ class Api::ApiController < ApplicationController
         when 'user_comment_on_course'
           cretator = notification.notificator.user
           owner = notification.notificator.commentable
+          course = Course.find(notification.notificator.commentable_id)
         when 'new_delivery_on_course'
           cretator = notification.notificator.user
           owner = notification.notificator.course[0]
-          course = Course.last #Assignment.find_by_delivery_id_and_course_id(notification.notificator.id,@user.id).course
+          course = Course.find(notification.notificator.commentable_id) #Assignment.find_by_delivery_id_and_course_id(notification.notificator.id,@user.id).course
         when 'new_public_course_on_network'
           cretator = notification.notificator.user
         # when 'new_survey_on_course'
