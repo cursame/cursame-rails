5e120d0ad9be04e1f3536aed313adc2ac1fc41b2
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2013-04-22 12:49:39 -0500

    agregando cambio en nil courses

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130415193823) do
+ActiveRecord::Schema.define(:version => 20130419210005) do
 
   create_table "activities", :force => true do |t|
     t.string   "title"
@@ -98,9 +98,9 @@ ActiveRecord::Schema.define(:version => 20130415193823) do
     t.string   "role",                           :default => "comments"
     t.datetime "created_at",                                             :null => false
     t.datetime "updated_at",                                             :null => false
-    t.integer  "netwok_id"
     t.text     "comment_html"
     t.integer  "network_id"
+    t.integer  "course_id"
     t.integer  "likes"
   end
 
@@ -109,7 +109,6 @@ ActiveRecord::Schema.define(:version => 20130415193823) do
   add_index "comments", ["user_id"], :name => "index_comments_on_user_id"
 
   create_table "compart_assets", :force => true do |t|
-    t.string   "asset"
     t.integer  "asset_id"
     t.integer  "delivery_id"
     t.integer  "assignment_id"
@@ -189,6 +188,17 @@ ActiveRecord::Schema.define(:version => 20130415193823) do
     t.datetime "updated_at",    :null => false
   end
 
+  create_table "errors", :force => true do |t|
+    t.integer  "number"
+    t.text     "message"
+    t.string   "os"
+    t.string   "browser"
+    t.datetime "create_at"
+    t.integer  "importance"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
   create_table "events", :force => true do |t|
     t.string   "title"
     t.datetime "starts_at"
@@ -222,10 +232,23 @@ ActiveRecord::Schema.define(:version => 20130415193823) do
     t.datetime "updated_at", :null => false
   end
 
+  add_index "friendships", ["user_id", "friend_id"], :name => "index_friendships_on_user_id_and_friend_id", :unique => true
+
   create_table "groups", :force => true do |t|
     t.integer  "user_id"
     t.string   "name"
     t.text     "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  create_table "libraries", :force => true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "gade_e"
+    t.boolean  "active"
+    t.string   "file"
+    t.integer  "network_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
   end
