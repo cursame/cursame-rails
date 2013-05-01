0c7ea28f1bf3c07f80ddbc4da90203e794fdd00e
Emiliano Cabrera <jemiliano.cabrera@gmail.com>
2013-05-01 18:02:28 -0500

    Modificacion para Prueba

--- db/migrate/20130424222304_create_correct_deliveries_courses.rb
+++ db/migrate/20130424222304_create_correct_deliveries_courses.rb
@@ -1,5 +1,23 @@
 class CreateCorrectDeliveriesCourses < ActiveRecord::Migration
   def up
+    
+    if (ActiveRecord::Base.connection.table_exists?("deliveries_courses")) then
+      drop_table :deliveries_courses
+    end
+    create_table :deliveries_courses, :id => false do |t|
+      t.integer :course_id
+      t.integer :delivery_id
+
+      t.timestamps
+    end
+    
+
+  end
+
+  def down
+
+
+
     table = nil
     if (ActiveRecord::Base.connection.table_exists?("deliveries_courses")) then
       table = DeliveriesCourse.all
@@ -15,11 +33,7 @@ class CreateCorrectDeliveriesCourses < ActiveRecord::Migration
         DeliveriesCourse.create!(:course_id => deliveries_course.course_id, :delivery_id => deliveries_courses.delivery_id)
       end
     end
-  end
 
-  def down
-    if (ActiveRecord::Base.connection.table_exists?("deliveries_courses")) then
-      drop_table :deliveries_courses
-    end
+
   end
 end
