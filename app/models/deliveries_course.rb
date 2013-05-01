e42cd4b28fb3d48374862e7393a9a7d5a3e2aab4
Emiliano Cabrera <jemiliano.cabrera@gmail.com>
2013-05-01 18:46:26 -0500

    Corregido el migrate para crear los deliveries_courses

--- app/models/deliveries_course.rb
+++ app/models/deliveries_course.rb
@@ -1,4 +1,6 @@
 class DeliveriesCourse < ActiveRecord::Base
+  attr_accessible :created_at, :updated_at, :course_id, :delivery_id
+
   belongs_to :course
   belongs_to :delivery, :dependent => :destroy
 end
--- db/migrate/20130424222304_create_correct_deliveries_courses.rb
+++ db/migrate/20130424222304_create_correct_deliveries_courses.rb
@@ -1,6 +1,26 @@
 class CreateCorrectDeliveriesCourses < ActiveRecord::Migration
   def up
-    
+    table = nil
+    if (ActiveRecord::Base.connection.table_exists?("deliveries_courses")) then
+      table = DeliveriesCourse.all
+      table = table.map {|x| x.attributes}
+      drop_table :deliveries_courses
+    end
+    create_table :deliveries_courses do |t|
+      t.integer :course_id
+      t.integer :delivery_id
+      t.timestamps
+    end
+
+    if !table.nil? then
+      table.each do |hash|
+        d = DeliveriesCourse.new(hash)
+        d.save!
+      end
+    end
+  end
+
+  def down
     if (ActiveRecord::Base.connection.table_exists?("deliveries_courses")) then
       drop_table :deliveries_courses
     end
@@ -10,30 +30,5 @@ class CreateCorrectDeliveriesCourses < ActiveRecord::Migration
 
       t.timestamps
     end
-    
-
-  end
-
-  def down
-
-
-
-    table = nil
-    if (ActiveRecord::Base.connection.table_exists?("deliveries_courses")) then
-      table = DeliveriesCourse.all
-      drop_table :deliveries_courses
-    end
-    create_table :deliveries_courses do |t|
-      t.integer :course_id
-      t.integer :delivery_id
-    end
-
-    if !table.nil? then
-      table.each do |deliveries_course|
-        DeliveriesCourse.create!(:course_id => deliveries_course.course_id, :delivery_id => deliveries_courses.delivery_id)
-      end
-    end
-
-
   end
 end
