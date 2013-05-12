cfbb09e8ba2e2e5b47fce37f498a453582baf907
waldix <waldix86@gmail.com>
2013-05-12 14:39:49 -0500

    Se suben notificaciones de comentarios para cursos.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -93,12 +93,17 @@ class Comment < ActiveRecord::Base
       course = notification_kind["course"] ? [commentable] : nil
 
       wall = Wall.create(:publication => self, :network => self.network, :users => users,:public => false, :courses => course)
+      member_in_courses = MembersInCourse.where(:course_id => commentable_id, :accepted => true)
 
-      users = users.reject{ |user| user.id == self.user_id }
-
-      users.each do |user|
+      member_in_courses.each do |member|
+        user = User.find(member.user_id)
         Notification.create(:user => user, :notificator => self, :kind => notification_kind)
       end
+      #users = users.reject{ |user| user.id == self.user_id }
+
+      #users.each do |user|
+        #Notification.create(:user => user, :notificator => self, :kind => notification_kind)
+      #end
       return
     elsif notification_kind["discussion"]
 
