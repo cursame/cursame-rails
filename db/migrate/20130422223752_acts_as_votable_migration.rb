b99da518bd5f77b36b15729a59fde13e4a085c11
jose armando gonzalez lopez <iam@armando.mx>
2013-05-13 01:56:51 -0500

    se comento act as votable para correr migrates

--- db/migrate/20130422223752_acts_as_votable_migration.rb
+++ db/migrate/20130422223752_acts_as_votable_migration.rb
@@ -1,24 +1,24 @@
 class ActsAsVotableMigration < ActiveRecord::Migration
-  def self.up
+  # def self.up
   
-    create_table :votes do |t|
+  #   create_table :votes do |t|
 
-      t.references :votable, :polymorphic => true
-      t.references :voter, :polymorphic => true
+  #     t.references :votable, :polymorphic => true
+  #     t.references :voter, :polymorphic => true
 
-      t.boolean :vote_flag
-      t.string :vote_scope
+  #     t.boolean :vote_flag
+  #     t.string :vote_scope
 
-      t.timestamps
-    end
+  #     t.timestamps
+  #   end
 
-    add_index :votes, [:votable_id, :votable_type]
-    add_index :votes, [:voter_id, :voter_type]
-    add_index :votes, [:voter_id, :voter_type, :vote_scope]
-    add_index :votes, [:votable_id, :votable_type, :vote_scope]
-  end
+  #   add_index :votes, [:votable_id, :votable_type]
+  #   add_index :votes, [:voter_id, :voter_type]
+  #   add_index :votes, [:voter_id, :voter_type, :vote_scope]
+  #   add_index :votes, [:votable_id, :votable_type, :vote_scope]
+  # end
 
-  def self.down
-    drop_table :votes
-  end
+  # def self.down
+  #   drop_table :votes
+  # end
 end
