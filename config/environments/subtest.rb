a88619ec708dfb4bcb0ca90db286fe3314807c0c
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2013-05-21 18:46:10 -0500

    subiendo return enviromenr

--- config/environments/subtest.rb
+++ config/environments/subtest.rb
@@ -64,7 +64,7 @@ Cursame30Lb::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   HOST = "cursalab.com"
-  config.action_mailer.default_url_options = { :host => "cursalab.com" }
+  config.action_mailer.default_url_options = { :host => 'cursalab.com' }
   # Action Mailer
   ActionMailer::Base.smtp_settings = {
     :address        => '50.116.21.144',
