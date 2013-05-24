c6d18e34b64fbf0b9c0fc2a4ef159626b7336b8f
jose armando gonzalez lopez <iam@armando.mx>
2013-05-23 20:29:57 -0500

    se corrigio migrate erronedo

--- db/migrate/20130404225033_add_publish_column_to_survey.rb
+++ db/migrate/20130404225033_add_publish_column_to_survey.rb
@@ -2,7 +2,7 @@ class AddPublishColumnToSurvey < ActiveRecord::Migration
   def change
     add_column :surveys, :publish, :boolean
   end
-  def change
+  def down
     remove_column :surveys, :publish
   end
 end
