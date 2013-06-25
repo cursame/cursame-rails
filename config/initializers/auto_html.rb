485952a72247d0fc59a73f2ac3e422c3465848a2
jose armando gonzalez lopez <iam@armando.mx>
2013-06-25 18:16:35 -0500

    se corrigio el eror de videos de youtube

--- config/initializers/auto_html.rb
+++ config/initializers/auto_html.rb
@@ -190,16 +190,16 @@ AutoHtml.add_filter(:youtube).with(:width => 420, :height => 315, :frameborder =
     wmode = options[:wmode]
     src = "//www.youtube.com/embed/#{youtube_id}"
     src += "?wmode=#{wmode}" if wmode
-    %{<iframe width="#{width}" height="#{height}" src="#{src}" frameborder="#{frameborder}" allowfullscreen></iframe>}
+    %{<iframe width="#{width}" height="#{height}" src="#{src}?wmode=transparent" frameborder="#{frameborder}" allowfullscreen></iframe>}
   end
 end
 
-AutoHtml.add_filter(:youtube_js_api).with(:width => 390, :height => 250) do |text, options|
-  text.gsub(/https?:\/\/(www.)?youtube\.com\/watch\?v=([A-Za-z0-9._%-]*)(\&\S+)?/) do
-    youtube_id = $2
-    %{<object width="#{options[:width]}" height="#{options[:height]}"><param name="movie" value="//www.youtube.com/v/#{youtube_id}?enablejsapi=1&playerapiid=ytplayer"></param><param name="wmode" value="transparent"></param><param name="allowscriptaccess" value="always"></param><embed src="//www.youtube.com/v/#{youtube_id}?enablejsapi=1&playerapiid=ytplayer" type="application/x-shockwave-flash" wmode="transparent" width="#{options[:width]}" height="#{options[:height]}" allowscriptaccess="always"></embed></object>}
-  end
-end
+# AutoHtml.add_filter(:youtube_js_api).with(:width => 390, :height => 250) do |text, options|
+#   text.gsub(/https?:\/\/(www.)?youtube\.com\/watch\?v=([A-Za-z0-9._%-]*)(\&\S+)?/) do
+#     youtube_id = $2
+#     %{<object width="#{options[:width]}" height="#{options[:height]}"><param name="movie" value="//www.youtube.com/v/#{youtube_id}?enablejsapi=1&playerapiid=ytplayer"></param><param name="wmode" value="transparent"></param><param name="allowscriptaccess" value="always"></param><embed src="//www.youtube.com/v/#{youtube_id}?enablejsapi=1&playerapiid=ytplayer" type="application/x-shockwave-flash" wmode="transparent" width="#{options[:width]}" height="#{options[:height]}" allowscriptaccess="always"></embed></object>}
+#   end
+# end
 
 AutoHtml.add_filter(:ustream_support).with(:width => 400) do |text, options|
   text.gsub(/https?:\/\/(www\.|)ustream\.tv\/channel\/(.+)/) do
