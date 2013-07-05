61a0184b7bdef4e06c4caaa057b0fc6eab52c76f
Emiliano <jemiliano.cabrera@gmail.com>
2013-07-04 20:21:47 -0500

    Possible_friends posible solucion

--- app/controllers/networks_controller.rb
+++ app/controllers/networks_controller.rb
@@ -181,7 +181,7 @@ class NetworksController < ApplicationController
     }
     @network_users = @possible_friends + @friends + @inverse_friends
     @network_users = @network_users.sort { |x,y| x[0].to_s <=> y[0].to_s }
-    @network_users = @network_users.reject {|array| array[0].nil? }
+    # @network_users = @network_users.reject {|array| array[0].nil? }
 
     @puts_possible = @possible_friends.map{ |x| x.first.email }
     @puts_friends = @friends.map{ |x| x.first.email}
