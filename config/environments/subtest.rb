8b2f740e3ddf0d592603bd7afa71e539e1292f4a
Emiliano <jemiliano.cabrera@gmail.com>
2013-07-22 15:19:59 -0500

    Confirguracion de enviroment

--- config/environments/subtest.rb
+++ config/environments/subtest.rb
@@ -63,7 +63,7 @@ Cursame30Lb::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  HOST = "cursalab.com"
+  HOST = "cursa.me"
   config.action_mailer.default_url_options = { :host => 'cursa.me' }
   # Action Mailer
   ActionMailer::Base.smtp_settings = {
