f097938ada71443d8ce64f2cd02e9c490e97afae
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2013-07-21 23:31:42 -0500

    Revert "Revert "resto de los cambios no hacer merge no listos""
    This reverts commit 10e3f5815158d3b1c001de6602b6424235c451dd.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130704231058) do
+ActiveRecord::Schema.define(:version => 20130716192504) do
 
   create_table "activities", :force => true do |t|
     t.string   "title"
@@ -115,6 +115,7 @@ ActiveRecord::Schema.define(:version => 20130704231058) do
     t.datetime "updated_at",                                             :null => false
     t.text     "comment_html"
     t.integer  "network_id"
+    t.integer  "course_id"
     t.integer  "likes"
   end
 
@@ -123,7 +124,6 @@ ActiveRecord::Schema.define(:version => 20130704231058) do
   add_index "comments", ["user_id"], :name => "index_comments_on_user_id"
 
   create_table "compart_assets", :force => true do |t|
-    t.string   "asset"
     t.integer  "asset_id"
     t.integer  "delivery_id"
     t.integer  "assignment_id"
@@ -199,10 +199,8 @@ ActiveRecord::Schema.define(:version => 20130704231058) do
   end
 
   create_table "deliveries_courses", :force => true do |t|
-    t.integer  "course_id"
-    t.integer  "delivery_id"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.integer "course_id"
+    t.integer "delivery_id"
   end
 
   create_table "delivery_assets", :force => true do |t|
@@ -228,6 +226,17 @@ ActiveRecord::Schema.define(:version => 20130704231058) do
     t.integer  "course_id"
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
+  end
+
+  create_table "errors", :force => true do |t|
+    t.integer  "number"
+    t.text     "message"
+    t.string   "os"
+    t.string   "browser"
+    t.datetime "create_at"
+    t.integer  "importance"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "events", :force => true do |t|
@@ -263,10 +272,23 @@ ActiveRecord::Schema.define(:version => 20130704231058) do
     t.datetime "updated_at", :null => false
   end
 
+  add_index "friendships", ["user_id", "friend_id"], :name => "index_friendships_on_user_id_and_friend_id", :unique => true
+
   create_table "groups", :force => true do |t|
     t.integer  "user_id"
     t.string   "name"
     t.text     "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  create_table "libraries", :force => true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "gade_e"
+    t.boolean  "active"
+    t.string   "file"
+    t.integer  "network_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
   end
@@ -309,11 +331,11 @@ ActiveRecord::Schema.define(:version => 20130704231058) do
   create_table "networks", :force => true do |t|
     t.string   "name"
     t.string   "subdomain"
-    t.datetime "created_at",                        :null => false
-    t.datetime "updated_at",                        :null => false
     t.integer  "population"
-    t.boolean  "public_register", :default => true
-    t.boolean  "free",            :default => true
+    t.boolean  "public_register"
+    t.boolean  "free"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
   create_table "networks_users", :force => true do |t|
@@ -331,6 +353,13 @@ ActiveRecord::Schema.define(:version => 20130704231058) do
     t.datetime "created_at",                         :null => false
     t.datetime "updated_at",                         :null => false
     t.boolean  "active",           :default => true
+  end
+
+  create_table "p_id_to_h_ids", :force => true do |t|
+    t.integer  "p_id"
+    t.integer  "h_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "permissionings", :force => true do |t|
