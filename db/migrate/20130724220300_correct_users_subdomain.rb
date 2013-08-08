9e233fac81012127a928f355ab40eb906d9f2078
Emiliano <jemiliano.cabrera@gmail.com>
2013-08-08 16:43:41 -0500

    Corregido el migrate

--- db/migrate/20130724220300_correct_users_subdomain.rb
+++ db/migrate/20130724220300_correct_users_subdomain.rb
@@ -1,12 +1,14 @@
 class CorrectUsersSubdomain < ActiveRecord::Migration
   def up
     users = User.all
-    users.each do |user|
-      if user.subdomain.nil? then
-        if user.networks.last.nil? then
-          user.update_attributes(:subdomain => Network.last.subdomain)
-        else
-          user.update_attributes(:subdomain => user.networks.last.subdomain)
+    if Network.all.size != 0 then
+      users.each do |user|
+        if user.subdomain.nil? then
+          if user.networks.last.nil? then
+            user.update_attributes(:subdomain => Network.last.subdomain)
+          else
+            user.update_attributes(:subdomain => user.networks.last.subdomain)
+          end
         end
       end
     end
