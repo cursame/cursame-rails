11182a235d3e983d0a94c2c763fbaeb80b49d26c
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2013-09-08 19:33:18 -0500

    changes from time out for uploader

--- app/controllers/course_files_controller.rb
+++ app/controllers/course_files_controller.rb
@@ -10,10 +10,7 @@ class CourseFilesController < ApplicationController
   def create
     @course_file = CourseFile.new(params[:course_file])
     @course_id = (params[:course_id])
-    if @course_file.save
-     @cf_ui =  CourseFileIdUserId.create(:user_id => current_user.id, :course_file_id => @course_file.id )
-     @cf_ci =  CourseIdCourseFileId.create(:course_id => @course_id, :course_file_id => @course_file.id ) 
-    end
+   
     @file = "#{@course_file.file}"
     @split_name =  @file.split('/').last
     @termination = @file.split('.').last
@@ -23,6 +20,17 @@ class CourseFilesController < ApplicationController
     @sintetic_name = @split_name
     ##### encontrando al owner del curso
     @member = MembersInCourse.find_by_course_id_and_user_id(@course_id, current_user.id)
+    
+     if @course_file.save
+       @cf_ui =  CourseFileIdUserId.create(:user_id => current_user.id, :course_file_id => @course_file.id )
+       @cf_ci =  CourseIdCourseFileId.create(:course_id => @course_id, :course_file_id => @course_file.id ) 
+          respond_to do |format|
+             format.json
+             format.js 
+           end
+      end
+    
+         
     
   end
 
