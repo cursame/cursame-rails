915cd52eac8babf5bb098aef01cd493d31b3b9eb
jose armando gonzalez lopez <iam@armando.mx>
2013-09-18 16:34:11 -0500

    se agrego la validacion para las notificaciones no se encimen y tambien cuando se comenta en el perfil del usuario

--- app/assets/javascripts/application.notifications.js
+++ app/assets/javascripts/application.notifications.js
@@ -1,7 +1,27 @@
 $(function(){
 	$('#notifications_count').on('click',function(e){
 		e.preventDefault();
-		$('#box-notifications').toggle();
+		$("#box-messages-notifications").hide();
+		$('#box-notifications').toggle();		
 	});
-});
+
+	$("#messages-notifications-count").click(function(e) {
+		e.preventDefault();
+		$("#box-notifications").hide();
+		$("#box-messages-notifications").toggle();
+	});
+	    //mostrar online
+	    $("#toggle-chat").click(function () {
+	    	var usersOnlinePanelPosition = $('#users-and-groups-for-chat').css('bottom');
+
+	    	if(usersOnlinePanelPosition.replace('px','') < 200){
+	    		$('#users-and-groups-for-chat').css('bottom','312px');
+	    	}
+	    	else{
+	    		$('#users-and-groups-for-chat').css('bottom','1px');
+	    	}
+	    	$("#users-and-groups-online").toggle();	    	
+	    });
+
+	});
 
