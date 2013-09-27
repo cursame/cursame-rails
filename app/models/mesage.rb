00c8860d1ff0504b63f3250ed2d69420d1927629
jose armando gonzalez lopez <iam@armando.mx>
2013-09-27 14:37:05 -0500

    se agrego un private pub, para los mensajes del usuario

--- app/models/mesage.rb
+++ app/models/mesage.rb
@@ -37,6 +37,12 @@ class Mesage < ActiveRecord::Base
 		       reciever: user,
 		       channel: self.channel
 		    )
+		    PrivatePub.publish_to(self.channel.channel_name+user.id.to_s,
+		       message: self,
+		       sender: self.user,
+		       reciever: user,
+		       channel: self.channel
+		    )
 		end
   end
 end
