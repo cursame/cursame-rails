e1b2f95ec1dd2069a26edb0deb0b027c8430f1bc
Emiliano <jemiliano.cabrera@gmail.com>
2013-10-03 17:29:27 -0500

    Import Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -422,6 +422,11 @@ class User < ActiveRecord::Base
       if !errors then
         begin
           user.save!
+          per = Permissioning.new
+          per.role_id = role_id.to_i
+          per.network_id = network_id.to_i
+          per.user_id = (User.find_by_email(user.email)).id
+          per.save
         rescue ActiveRecord::RecordInvalid => invalid
           invalid.record.errors.each do |error|
             arrayErrores.push({:line => count, :message => "Falta especificar: " + error.to_s})
@@ -432,11 +437,6 @@ class User < ActiveRecord::Base
         #else
           # user.confirm!
           # user.save!
-          p = Permissioning.new
-          p.role_id = role_id.to_i
-          p.network_id = network_id.to_i
-          p.user_id = (User.find_by_email(user.email)).id
-          p.save
           #Permissioning.create(:role_id => role_id.to_i,:network_id => network_id.to_i, :user_id => user.id)
           # mail = Notifier.send_password(user,password)
           # mail.deliver
