d77fa73013b3ce6616547f9240f13875d9bd7627
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2013-11-22 17:32:49 -0600

    solucionando cambios en plataforma

--- db/schema.rb
+++ db/schema.rb
@@ -113,6 +113,7 @@ ActiveRecord::Schema.define(:version => 20131121223252) do
     t.string   "role",                           :default => "comments"
     t.datetime "created_at",                                             :null => false
     t.datetime "updated_at",                                             :null => false
+    t.integer  "netwok_id"
     t.text     "comment_html"
     t.integer  "network_id"
     t.integer  "likes"
