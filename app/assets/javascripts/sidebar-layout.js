7c8017033698ae35fe1f8fbc1cbc7678d5869243
Salvador Lopez Mendoza <simiographics@gmail.com>
2014-01-14 14:29:04 -0600

    Auto adjust js script for sidebar

--- app/assets/javascripts/sidebar-layout.js
+++ app/assets/javascripts/sidebar-layout.js
@@ -1,18 +1,28 @@
 $(function() {
 
+  /* Adjuts the sidebar components depending on the window Height
+  ----------------------------------*/
   var sidebar       = $('.global-sidebar'),
       user          = sidebar.find('div.user'),
       nav           = sidebar.find('div.sidebar-navigation'),
       courses       = sidebar.find('div.sidebar-courses'),
+      coursesHeader = courses.find('div.sidebar-section-header').outerHeight(),
       chat          = sidebar.find('div.sidebar-chat'),
+      chatHeader    = chat.find('div.sidebar-section-header').outerHeight(),
       footer        = sidebar.find('div.footer'),
-      staticHeight  = user.outerHeight() + nav.outerHeight() + footer.outerHeight();
+      staticHeight  = user.outerHeight() + nav.outerHeight() + footer.outerHeight(),
 
-  console.log(staticHeight);
+      target1       = courses.find('.sidebar-section-main'),
+      target2       = chat.find('.sidebar-section-main');
 
   function adjustSidebar() {
     var winHeight   = $(window).height();
-    sidebar.height(winHeight);      
+
+    availableHeight = winHeight - staticHeight - coursesHeader - chatHeader;
+    sidebar.height(winHeight);
+
+    target1.height(availableHeight/2);
+    target2.height(availableHeight/2)
   }
 
   adjustSidebar();
