9e6988d78526d01b73a3f216a37c8a95476508dd
jose armando gonzalez lopez <iam@armando.mx>
2014-01-28 16:48:25 -0600

    se agregaron las plantillas para los modales de ver cuestionario, survey, delivery

--- app/assets/javascripts/application.modal.js
+++ app/assets/javascripts/application.modal.js
@@ -1,12 +1,12 @@
 $(function() {
-	$('a').click(function() {
-		var me = $(this);
-		if (me.attr('data') === 'modal') {
-			$('.modalia').hide(); //si existe algún modalia abierto 
-			$('body').append('<div class="overlayia"></div>').show();
-			me.closest("div").find('.modalia').show();
-		}
-	});
+	window.createModal = function (content) {
+		$('.modalia').hide(); //si existe algún modalia abierto
+		$('.overlayia').hide(); //si existe algún modalia abierto
+		$('body').append('<div class="overlayia"></div>').show();
+		$('body').append('<div class="modalia" ></div>').show();
+		$('.modalia').html(content);
+	};
+		
 	$('.overlayia').live('click',function () {
 		$('.modalia').hide();
 		$(this).hide();
