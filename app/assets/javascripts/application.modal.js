b0ac757f804332e484525475fdd89c0481f38432
Salvador Lopez Mendoza <simiographics@gmail.com>
2014-01-31 11:20:02 -0600

    Delivery publication overlay

--- app/assets/javascripts/application.modal.js
+++ app/assets/javascripts/application.modal.js
@@ -6,27 +6,50 @@ $(function() {
 				overlayHeight = overlay.outerHeight(),
 				overlayWidth 	= overlay.outerWidth();
 
-		overlay.css({
-			'left': winWidth/2 - overlayWidth/2,
-			'top': winHeight/2 - overlayHeight/2
-		})
+		if ( winHeight > overlayHeight ) {
+			overlay.removeClass('not-height')
+			overlay.css({
+				'left': winWidth/2 - overlayWidth/2,
+				'top': winHeight/2 - overlayHeight/2
+			});
+		} else {
+			overlay.addClass('not-height');
+			overlay.css({
+				'left': 0,
+				'top': 0
+			})
+		}
 	}
 	
 	window.createModal = function (content) {
-		$('body').append('<div class="overlay-wrapper"></div>').css("overflow","hidden");
+		$('body').addClass('overlay-open').append('<div class="overlay-wrapper"></div>');
 		$('div.overlay-wrapper').append('<div class="overlay-screen"></div>').show().append('<div class="overlay" ></div>').show();
+		$('div.overlay-wrapper').css("overflow-y", "scroll");
 		$('.overlay').html(content);
 
 		overlayPositioning( $('.overlay') );
+
 	};
 	
 	$('.overlay-screen, .close-overlay').live('click',function () {
 		$('.overlay-wrapper').remove();
-		$('body').css("overflow", "");
+		$('body').removeClass('overlay-open');
 	});
 
 	$(window).on('resize', function() {
-    overlayPositioning( $('.overlay') );
+    overlayPositioning( $('div.overlay') );
+  });
+
+	/* Show delivery submit form */
+  $('.show-delivery-form').live('click', function(e) {
+    var $this = $(this),
+        target = $this.data('target-id');
+
+    $this.closest('.delivery-overlay-content').hide();
+    $('div#' + target).show();
+
+    overlayPositioning( $('div.overlay') );    
+    e.preventDefault();
   });
 
 });
