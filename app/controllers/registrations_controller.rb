aae9a136a587f1239a1710932f98e10b9007c1da
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2014-02-21 16:10:04 -0600

    cambios en redirect de user en update

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,27 +1,31 @@
 class RegistrationsController < Devise::RegistrationsController
   def update
+
     # required for settings form to submit when password is left blank
     if params[:user][:password].blank?
       params[:user].delete("password")
       params[:user].delete("password_confirmation")
+      #redirect_to show_user_path(current_user.personal_url)
     end
     if !params[:user][:user_id].blank?
       @user = User.find(params[:user][:user_id])
       user = User.find(current_user.id)
       params[:user].delete("user_id")
 
-
+      
 
       if @user.update_attributes(params[:user])
-        set_flash_message :notice, :updated
+        #set_flash_message :notice, :updated
+        flash[:notice] = "Se a actualizado correctamente tu perfil"
         # Sign in the user bypassing validation in case his password changed
         if (user.id == @user.id)
           sign_in @user, :bypass => true
         end
-        redirect_to :back
       else
+        flash[:notice] = "El perfil no pudo actualizarce correctamente"
         render "edit"
       end
+      
     else
       super
     end
@@ -32,5 +36,9 @@ class RegistrationsController < Devise::RegistrationsController
 
   def after_inactive_sign_up_path_for(resource)
     return "/awaiting_confirmation/#{resource.personal_url}"
+  end
+
+  def after_update_path_for(resource)
+      show_user_path(resource.personal_url)
   end
 end
