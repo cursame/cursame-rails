ec25cfd63e71cc78bcf6355990174afe94826ffe
Salvador Lopez Mendoza <simiographics@gmail.com>
2014-02-25 12:39:52 -0600

    Correcion en application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,67 +21,63 @@
 
 // Adding and removing questions answers
 function remove_fields(link, toId) {
-    console.log(toId);
-    if(toId == '#box-question') {
-        $(link).closest('.question-field').remove();
-        changeNumbers('#box-question', '#question-num');
-    } else if( toId =='#box-request' ) {
-        var grandfather = $(link).parent().parent().parent();
-        $(link).parent().parent().remove();
-        changeNumbers(grandfather, '#request-num');
-    }
-}
+  
+  if(toId == '#box-question') {
+    $(link).closest('.question-field').remove();
+    changeNumbers('#box-question', '#question-num');
+  } else if( toId =='#box-request' ) {
+    var grandfather = $(link).parent().parent().parent();
+    $(link).parent().parent().remove();
+    changeNumbers(grandfather, '#request-num');
+  }
+};
 
 function add_fields(link, association, content, toId) {
-    //toId catch:
-    //  #box-request
-    //  #box-question
 	var new_id = new Date().getTime();
 	var regexp = new RegExp("new_" + association, "g");
-    if(toId =='#box-question'){
-        $(toId).append(content.replace(regexp, new_id));
-        changeNumbers('#box-question', '#question-num');
-    }else if( toId =='#box-request' ){
-        $(link).parent().parent().find('#box-request').append(content.replace(regexp, new_id));
-        changeNumbers($(link).parent().parent().find('#box-request'), '#request-num');
-    }
 
-    $(document).ready(function(){
-        $('textarea').autosize();   
-    });
-}
+  if ( toId =='#box-question' ) {
+    $(toId).append(content.replace(regexp, new_id));
+    changeNumbers('#box-question', '#question-num');
+  }else if( toId =='#box-request' ){
+    $(link).parent().parent().find('#box-request').append(content.replace(regexp, new_id));
+    changeNumbers($(link).parent().parent().find('#box-request'), '#request-num');
+  }
+
+  $(document).ready(function(){
+      $('textarea').autosize();   
+  });
+};
 
 function changeNumbers(idParent, idFind){
-    var count = 0 ;
-    var alphabet= new Array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
-    if(idFind == '#question-num'){
-        $.each($(idParent).children(), function(index, value) {
-            $(value).find('#question-num').html(count+1+'. ');
-            count ++;
-        });
-    }else if( idFind =='#request-num' ){
-        $.each(idParent.children(), function(index, value) {
-            $(value).find('#request-num').html(alphabet[count]+') ');
-            //console.log($(value).attr('id'));
-            count ++;
-        });
-    }
-    var count = 0 ;
-}
+  var count = 0 ;
+  var alphabet= new Array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
 
- function playSound(soundfile) {
-    soundfile = "./assets/sounds/new_message.mp3";
-    document.getElementById("chat_sounds").innerHTML="<embed src=\""+soundfile+"\" hidden=\"true\" autostart=\"true\" loop=\"false\" />";
-    // document.getElementById("chat_sounds").innerHTML='<audio id="new_message_sound" src="+soundfile+" autoplay></audio>';
+  if ( idFind == '#question-num' ) {
+    $.each($(idParent).children(), function(index, value) {
+      $(value).find('#question-num').html(count+1+'. ');
+      count ++;
+    });
 
-    // document.getElementById('new_message_sound').play();
-    // alert(1232);
-    setTimeout(function () {
-        document.getElementById("chat_sounds").innerHTML="";
-    },1000);
- }
+  } else if( idFind =='#request-num' ){
+    $.each(idParent.children(), function(index, value) {
+      $(value).find('#request-num').html(alphabet[count]+') ');
+      count ++;
+    });
+  }
 
-// Truncate for javascript
+  var count = 0;
+};
+
+function playSound(soundfile) {
+  soundfile = "./assets/sounds/new_message.mp3";
+  document.getElementById("chat_sounds").innerHTML="<embed src=\""+soundfile+"\" hidden=\"true\" autostart=\"true\" loop=\"false\" />";
+
+  setTimeout(function () {
+      document.getElementById("chat_sounds").innerHTML="";
+  },1000);
+};
+
 function truncate(text, maxLength, ellipseText){
   ellipseText = ellipseText || '&hellip;';
 
@@ -100,98 +96,87 @@ function truncate(text, maxLength, ellipseText){
   
   //Make sure we do not just return a letter..
   return (lastCharPosition ? text.substr(0, lastCharPosition+1) : '') + ellipseText;
-}
+};
 
 // notificaciones push usando private_pub
 $(function() {
-    // data.sender.id = data.reciver.id return
+  // data.sender.id = data.reciver.id return
 	// Se suscribe al canal para las notificaciones del chat
-    
-    if (typeof Cursame === 'undefined') {
-      return;
-    }
+  if (typeof Cursame === 'undefined') {
+    return;
+  }
 
 	PrivatePub.subscribe ("/messages/notifications_user_" + Cursame.userId, function(data, channel) {
-        var url, notification, numNotifications;
+    var url, notification, numNotifications;
 		var channelType = data.channel.channel_name.split('course_channel_');
 		url = channelType[1] ? '/home/open_channel/' + channelType[1] + '?course=true' : '/home/open_channel/' + data.sender.id;		
+    var avatar_modern = data.sender.avatar.modern.url;
     
-        var avatar_modern = data.sender.avatar.modern.url 
+    if ( data.sender.id == Cursame.userId ){
+      return;
+    } else {
+      
+      notification = [
+          '<li class="unread">',
+          '<div class="activity-feed">',
+          '<a title="Conversar" href="' + url + '" data-remote="true">',
+          '<div class="activity-feed-author">',
+          '<img src="' + avatar_modern + '" class="avatar avatar-45"></div>',
+          '<div class="activity-feed-main">',
+          '<h6>' + data.sender.first_name+' '+ data.sender.last_name + '</h6>',
+          '<p>' + truncate(data.message.mesage, 88, "...") +'</p>',
+          '<span class="meta">' + jQuery.timeago(data.message.created_at) + '</span></div></a></div></li>'
+      ];
+      
+      playSound();
 
-        if (data.sender.id == Cursame.userId){
-           return;
+  		//si ya existe la conversacion no se crea la notificación
+  		if( $('#chat-channel-'+data.channel.id).length ) {
+  		  var panel = $("#chat-channel-" + data.channel.id);
+  	    var chatZonePosition = $(panel).css('bottom');
 
-           }else{
-
-        notification = [
-            '<li class="unread">',
-            '<div class="activity-feed">',
-            '<a title="Conversar" href="' + url + '" data-remote="true">',
-            '<div class="activity-feed-author">',
-            '<img src="' + avatar_modern + '" class="avatar avatar-45"></div>',
-            '<div class="activity-feed-main">',
-            '<h6>' + data.sender.first_name+' '+ data.sender.last_name + '</h6>',
-            '<p>' + truncate(data.message.mesage, 88, "...") +'</p>',
-            '<span class="meta">' + jQuery.timeago(data.message.created_at) + '</span></div></a></div></li>'
-        ];
-
-        // reproducimos sonido cuando hay mensaje
-        playSound();
-
-		//si ya existe la conversacion no se crea la notificación
-		if($('#chat-channel-'+data.channel.id).length) {
-			var panel = $("#chat-channel-" + data.channel.id);
-	    	var chatZonePosition = $(panel).css('bottom');
-
-            // aqui agrego un asterisco a la ventanita
-            // cuando esta collapsada, asi sabe cuando el usario tiene un msj pendiente esa ventana
-            if (chatZonePosition.replace('px', '') < 200) {
-                $("#chat-channel-" + data.channel.id + "span").append('*');
-            }
-            // alineamos los comentarios que no son de nosotros a la derecha
-            setTimeout(function () {
-                //$('#message_' + data.message.id).css('float','right');
-            }, 200);
-            
-		} else {
-			numNotifications = $('#messages-notifications-count').html() * 1;
-			$('#messages-notifications-count').html(numNotifications + 1);
-            $('#messages-notifications-count').show();
-			$('#messages-notifications-list').prepend(notification.join(''));
-		}
-	});
+        // aqui agrego un asterisco a la ventanita
+        // cuando esta collapsada, asi sabe cuando el usario tiene un msj pendiente esa ventana
+        if (chatZonePosition.replace('px', '') < 200) {
+            $("#chat-channel-" + data.channel.id + "span").append('*');
+        }     
+  		} else {
+  			numNotifications = $('#messages-notifications-count').html() * 1;
+  			$('#messages-notifications-count').html(numNotifications + 1);
+        $('#messages-notifications-count').show();
+  			$('#messages-notifications-list').prepend(notification.join(''));
+  		}
+    }
+  });
     
-    PrivatePub.subscribe("/messages/chat_notifications", function(data, channel) {
-        if (data && data.online) {
-            $('#chat-list-user-' + data.userId).removeClass('user-offline').addClass('user-online');
-        } else {
-            $('#chat-list-user-' + data.userId).removeClass('user-online').addClass('user-offline');
-        }
-    });
-    // Scroll top
-    $(window).scroll(function(){
-      if ($(this).scrollTop() > 100) {
-        $('#scrollup').fadeIn();
-      } else {
-        $('#scrollup').fadeOut();
-      }
-    });
+  PrivatePub.subscribe("/messages/chat_notifications", function(data, channel) {
+    if ( data && data.online ) {
+      $('#chat-list-user-' + data.userId).removeClass('user-offline').addClass('user-online');
+    } else {
+      $('#chat-list-user-' + data.userId).removeClass('user-online').addClass('user-offline');
+    }
+  });
 
-    $('#scrollup').click(function() {
-      $('html,body').animate({ scrollTop: 0 }, '1500', 'swing', function() { 
-      });
-    
-      return false;
-    });
-});
+  // Scroll top
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 100) {
+      $('#scrollup').fadeIn();
+    } else {
+      $('#scrollup').fadeOut();
+    }
+  });
 
+  $('#scrollup').click(function() {
+    $('html,body').animate({ scrollTop: 0 }, '1500', 'swing');
+  });
 
-$('.edit-publication-link').live('click', function() {
-  $(this).closest('.publication-box').find('.form_for_edit_wall').slideToggle(300);
-});
+  $('.edit-publication-link').live('click', function() {
+    $(this).closest('.publication-box').find('.form_for_edit_wall').slideToggle(300);
+  });
 
-$('.cancel-edit-publication').live('click', function(event) {
-  event.preventDefault();
-  $(this).closest('.form_for_edit_wall').slideUp(300);
+  $('.cancel-edit-publication').live('click', function(event) {
+    event.preventDefault();
+    $(this).closest('.form_for_edit_wall').slideUp(300);
+  });
 });
 
