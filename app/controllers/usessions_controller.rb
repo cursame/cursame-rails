b61c5f8089e09cc55f60dc52ef81e61839e4b0cd
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2014-02-27 14:11:35 -0600

    cambio en mensaje denotice de loggin

--- app/controllers/usessions_controller.rb
+++ app/controllers/usessions_controller.rb
@@ -17,9 +17,9 @@ class UsessionsController < Devise::SessionsController
     # POST /resource/sign_in
     def create      
       self.resource = warden.authenticate!(auth_options)
-      set_flash_message(:notice, :signed_in) if is_navigational_format?
-     
+     # set_flash_message(:notice, :signed_in) if is_navigational_format?
       @find_user = User.find_by_email(resource.email)
+      flash[:notice] = "Hola #{ @find_user.name } bienvenido de nuevo."
 
       @find_user.online = true
       @find_user.save!
