9470616f82ca3b4a177cd6c9a70ae4bb65e915f4
Salvador Mendo <simiographics@gmail.com>
2014-06-25 12:50:49 -0500

    profile design fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,11 +3,11 @@
 
 new_network = Network.create(name: 'pruebas', subdomain: 'pruebas', population: '300', public_register: true , free: true, register_form: true, welcom_message: 'Hola')
 
-admin      = User.create(:email => "luis+admin@cursa.me",      :password => "5949wh3aq", :first_name => "Luis Alfredo", :last_name => "Lorenzo Alva", :confirmed_at => Time.now, :personal_url => 'babis_admin',      :domain => 'cursa.me', :subdomain => 'pruebas')
-student    = User.create(:email => "luis+student@cursa.me",    :password => "5949wh3aq", :first_name => "Luis Alfredo", :last_name => "Lorenzo Alva", :confirmed_at => Time.now, :personal_url => 'babis_student',    :domain => 'cursa.me', :subdomain => 'pruebas')
-teacher    = User.create(:email => "luis+teacher@cursa.me",    :password => "5949wh3aq", :first_name => "Luis Alfredo", :last_name => "Lorenzo Alva", :confirmed_at => Time.now, :personal_url => 'babis_teacher',    :domain => 'cursa.me', :subdomain => 'pruebas')
-superadmin = User.create(:email => "luis+superadmin@cursa.me", :password => "5949wh3aq", :first_name => "Luis Alfredo", :last_name => "Lorenzo Alva", :confirmed_at => Time.now, :personal_url => 'babis_superadmin', :domain => 'cursa.me', :subdomain => 'pruebas')
-supervisor = User.create(:email => "luis+supervisor@cursa.me", :password => "5949wh3aq", :first_name => "Luis Alfredo", :last_name => "Lorenzo Alva", :confirmed_at => Time.now, :personal_url => 'babis_supervisor', :domain => 'cursa.me', :subdomain => 'pruebas')
+admin      = User.create(:email => "salvador+admin@cursa.me",      :password => "mmmmmm", :first_name => "Salvador", :last_name => "Mendo", :confirmed_at => Time.now, :personal_url => 'salvador_admin',      :domain => 'cursa.me', :subdomain => 'pruebas')
+student    = User.create(:email => "salvador+student@cursa.me",    :password => "mmmmmm", :first_name => "Salvador", :last_name => "Mendo", :confirmed_at => Time.now, :personal_url => 'salvador_student',    :domain => 'cursa.me', :subdomain => 'pruebas')
+teacher    = User.create(:email => "salvador+teacher@cursa.me",    :password => "mmmmmm", :first_name => "Salvador", :last_name => "Mendo", :confirmed_at => Time.now, :personal_url => 'salvador_teacher',    :domain => 'cursa.me', :subdomain => 'pruebas')
+superadmin = User.create(:email => "salvador+superadmin@cursa.me", :password => "mmmmmm", :first_name => "Salvador", :last_name => "Mendo", :confirmed_at => Time.now, :personal_url => 'salvador_superadmin', :domain => 'cursa.me', :subdomain => 'pruebas')
+supervisor = User.create(:email => "salvador+supervisor@cursa.me", :password => "mmmmmm", :first_name => "Salvador", :last_name => "Mendo", :confirmed_at => Time.now, :personal_url => 'salvador_supervisor', :domain => 'cursa.me', :subdomain => 'pruebas')
 
 Permissioning.create(:user_id => admin.id,      :role_id => "1", :network_id => new_network.id)
 Permissioning.create(:user_id => student.id,    :role_id => "2", :network_id => new_network.id)
