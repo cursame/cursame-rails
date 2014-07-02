c704e1e60c5732c9870e0f2f43809fc4610fb13e
Emiliano <jemiliano.cabrera@gmail.com>
2014-07-02 12:17:09 -0500

    Reescritos unos metodos

--- app/utilities/courses_utils.rb
+++ app/utilities/courses_utils.rb
@@ -9,17 +9,17 @@ module CoursesUtils
       course.owner?(Role.find_by_title("teacher"), current_user) and not(course.active_status)
     end
   end
-
+  
   def student_subscribed_courses
     current_user.courses.where(:network_id => current_network.id, :id => operator_courses('student') ,:active_status => true)
   end
-
+  
   def student_pending_requests
     members_in_courses = MembersInCourse.where(user_id: current_user.id).where("members_in_courses.accepted != ?", true)
     members_in_courses = members_in_courses.keep_if do |member| 
       member.course.network_id == current_network.id
     end
-    members_in_courses.map do |member| member.course end
+    members_in_courses.map{ |member| member.course}
   end
 
   def network_courses_not_subscribed
@@ -27,30 +27,25 @@ module CoursesUtils
   end
 
   def operator_courses(typed = 'normal')
-    ids =  []
+    ids = Array.new
+
     case typed 
-      when 'inverse'
-        courses_ids = current_user.members_in_courses.map do |member| member.course_id end
-        ids = current_network.courses.map do |course| course.id end - courses_ids
-
-      when 'normal'
-        ### coloca los ids de los cursos para comparacion
-        comparative_courses = []
-        current_user.courses.each do |c|
-          comparative_courses.push(c.id)
-        end
-        ids = comparative_courses
-
-      when 'student'
-        members = current_user.members_in_courses.keep_if do |member| member.accepted end
-        ids = members.map do |member| member.course_id end
-
-      when 'teacher'
-        courses = current_user.courses.keep_if do |course| course.owner?(Role.find_by_title("teacher"), current_user) end
-        ids = courses.map do |course| course.id end
+    when 'inverse'
+      courses_ids = current_user.members_in_courses.map {|member| member.course_id}
+      ids = current_network.courses.map{|course| course.id} - courses_ids
+      
+    when 'normal'
+      ids = current_user.courses.map {|course| course.id}
+      
+    when 'student'
+      members = current_user.members_in_courses.keep_if do |member| member.accepted end
+      ids = members.map do |member| member.course_id end
+      
+    when 'teacher'
+      courses = current_user.courses.keep_if do |course| course.owner?(Role.find_by_title("teacher"), current_user) end
+      ids = courses.map do |course| course.id end
     end
 
     ids 
   end
-
 end
