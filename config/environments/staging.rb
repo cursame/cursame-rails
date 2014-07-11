9a72b0b7252dbbd8c05184acc90457e0a2875011
Luis Alfredo <luis.alfredo@ciencias.unam.mx>
2014-07-11 13:31:12 -0500

    Configuración ambiente de staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -30,10 +30,10 @@ Cursame30Lb::Application.configure do
   # config.force_ssl = true
 
   # See everything in the log (default is :info)
-  config.log_level = :debug
+  # config.log_level = :debug
 
   # Prepend all log lines with the following tags
-  config.log_tags = [ :subdomain, :uuid ]
+  # config.log_tags = [ :subdomain, :uuid ]
 
   # Use a different logger for distributed setups
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
