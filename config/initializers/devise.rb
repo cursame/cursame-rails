966de874db77500a06e18274133e3c5752981d10
Luis Alfredo <luis.alfredo@ciencias.unam.mx>
2014-07-11 16:23:17 -0500

    se quita secret key de devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -232,6 +232,4 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = "/my_engine/users/auth"
 
-  config.secret_key = 'rsXCAQmyCs4r0eUHeDRlbBhFFBkWVGeCj'
-
 end
