8b2ef69d0cc21f076a16236dab9fa033416b558d
Luis Alfredo <luis.alfredo@ciencias.unam.mx>
2014-07-28 20:41:19 -0500

    Ajustes al enviar evento en Mixpanel

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -4,7 +4,6 @@ class CoursesController < ApplicationController
   filter_access_to :show
   before_filter :course_activated, :only => [:show, :about, :members, :library]
   include CoursesUtils
-  include MixpanelEventTracker
 
   def index
     @member = MembersInCourse.new
