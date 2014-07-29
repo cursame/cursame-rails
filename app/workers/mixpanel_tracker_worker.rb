c860b233d9253a97a2da5c91786d9befe6995ab8
Luis Alfredo <luis.alfredo@ciencias.unam.mx>
2014-07-28 20:41:19 -0500

    Ajustes al enviar evento en Mixpanel

--- app/workers/mixpanel_tracker_worker.rb
+++ app/workers/mixpanel_tracker_worker.rb
@@ -1,18 +1,18 @@
 require 'mixpanel-ruby'
 
-class MixpanelTrackerWorker 
-	include Sidekiq::Worker
-    
-    def mixpanel()
-        Mixpanel::Tracker.new(Settings.mixpanel.token)
-    end
+class MixpanelTrackerWorker
+  include Sidekiq::Worker
 
-    # A call to track is a report that an event has occurred
-    # http://mixpanel.github.io/mixpanel-ruby/Mixpanel/Tracker.html#method-i-track
-    def perform(distinct_id, event, properties={})
-        tracker = mixpanel()
-        tracker.track distinct_id, event, properties
-        puts "\e[1;32m[INFO]\e[0m sending event task to mixpanel \e[1;34m=>\e[0m user_id: #{distinct_id}, event: #{event}, properties: #{properties}"
-    end
+  def mixpanel()
+    Mixpanel::Tracker.new(Settings.mixpanel.token)
+  end
+
+  # A call to track is a report that an event has occurred
+  # http://mixpanel.github.io/mixpanel-ruby/Mixpanel/Tracker.html#method-i-track
+  def perform(distinct_id, event, properties={})
+    tracker = mixpanel()
+    tracker.track distinct_id, event, properties
+    puts "\e[1;32m[INFO]\e[0m sending event task to mixpanel \e[1;34m=>\e[0m user_id: #{distinct_id}, event: #{event}, properties: #{properties}"
+  end
 
 end
