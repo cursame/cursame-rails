dd6eb8c31ed396cc9d55aca7a3e588bf55d4a6f8
Manuel Lara <malh04@gmail.com>
2014-08-18 14:28:14 -0500

    Nueva migracion para agregar en response to the evaluation el campo de evaluation criteria

--- db/migrate/20140815003942_add_evaluation_criterium_id_to_response_to_evaluation.rb
+++ db/migrate/20140815003942_add_evaluation_criterium_id_to_response_to_evaluation.rb
@@ -1,11 +1,27 @@
 class AddEvaluationCriteriumIdToResponseToEvaluation < ActiveRecord::Migration
   def change
+
+  	add_column :response_to_the_evaluations, :evaluation_criterium, :integer
+    remove_column :response_to_the_evaluations, :evaluation_porcentage
+    add_index :response_to_the_evaluations, :evaluation_criterium
+
   	ResponseToTheEvaluation.all.each do |response|
-  	  criterium = EvaluationCriterium.find_by_evaluable_id_and_evaluable_type response.feedbackable_id, 'Assignment'
-  	  if criterium.nil?
-  	  	EvaluationCriterium.create name: response.name, evaluable_id: response.feedbackable_id, evaluable_type: 'Assignment'
+  	  assignment = Assignment.find_by_id response.feedbackable_id
+  	  evaluation_criteria = EvaluationCriterium.find_all_by_evaluable_id_and_evaluable_type assignment.delivery_id, 'Delivery'
+  	  evaluation_criteria.each do |elem|
+  	  	if response.name == elem.name
+  	  		response.update_attributes :evaluation_criteria, elem.id
+  	  	end
   	  end
   	end
-  	remove_column :response_to_the_evaluations, :name
+
+  	# ResponseToTheEvaluation.all.each do |response|
+  	#   criterium = EvaluationCriterium.find_by_evaluable_id_and_evaluable_type response.feedbackable_id, 'Delivery'
+  	#   if criterium.nil?
+  	#   	EvaluationCriterium.create name: response.name, evaluable_id: response.feedbackable_id, evaluable_type: 'Delivery'
+  	#   end
+  	#  end
+
+	remove_column :response_to_the_evaluations, :name
   end
 end
