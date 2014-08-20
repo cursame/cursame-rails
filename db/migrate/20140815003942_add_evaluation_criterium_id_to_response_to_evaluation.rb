e55fb7786c5a506c219b768d0ec3847367a7d9dd
Manuel Lara <malh04@gmail.com>
2014-08-20 12:25:38 -0500

    Se agregó información de debugeo para la migración

--- db/migrate/20140815003942_add_evaluation_criterium_id_to_response_to_evaluation.rb
+++ db/migrate/20140815003942_add_evaluation_criterium_id_to_response_to_evaluation.rb
@@ -5,11 +5,17 @@ class AddEvaluationCriteriumIdToResponseToEvaluation < ActiveRecord::Migration
     add_index :response_to_the_evaluations, :evaluation_criterium_id
 
     ResponseToTheEvaluation.all.each do |response|
+      puts "Response id: #{response.id} name: #{response.name}"
       assignment = Assignment.find_by_id response.feedbackable_id
-      evaluation_criteria = EvaluationCriterium.find_all_by_evaluable_id_and_evaluable_type assignment.delivery_id, 'Delivery'
+      puts "Assignment id: #{assignment.id} delivery: #{assignment.delivery_id}"
+      evaluation_criteria = EvaluationCriterium.find_all_by_evaluable_id_and_evaluable_type assignment.delivery_id, "Delivery"
       evaluation_criteria.each do |elem|
+        puts "Evaluation criteria id: #{evaluation_criteria.id} names: #{response.name} #{elem.name}"
         if response.name == elem.name
+          puts "DATA UPDATED"
           response.update_attributes :evaluation_criterium_id => elem.id
+        else
+          puts "Not found"
         end
       end
     end
