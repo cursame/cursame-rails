675dbd0c900d5669aee11690d5d90538e2456648
Luis Alfredo <luis.alfredo@ciencias.unam.mx>
2014-08-20 18:02:09 -0500

    Se corrige la forma de obtener promedio

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140815184000) do
+ActiveRecord::Schema.define(:version => 20140820223404) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "namespace"
@@ -485,17 +485,15 @@ ActiveRecord::Schema.define(:version => 20140815184000) do
   end
 
   create_table "response_to_the_evaluations", :force => true do |t|
-    t.string   "name"
     t.text     "comment_for_rubre"
     t.integer  "feedbackable_id"
-    t.integer  "evaluation_porcentage"
     t.datetime "created_at",              :null => false
     t.datetime "updated_at",              :null => false
     t.string   "feedbackable_type"
     t.integer  "evaluation_criterium_id"
   end
 
-  add_index "response_to_the_evaluations", ["evaluation_criterium_id"], :name => "index_response_to_the_evaluations_on_evaluation_criterium_id"
+  add_index "response_to_the_evaluations", ["evaluation_criterium_id"], :name => "index_response_to_the_evaluations_on_evaluation_criteium_id"
   add_index "response_to_the_evaluations", ["feedbackable_id", "feedbackable_type"], :name => "feedbackable_index"
 
   create_table "role_id_and_permission_ids", :force => true do |t|
