eec3031b0aa8e2c73036abe24e9ee312fbf0615e
Luis Alfredo <luis.alfredo@ciencias.unam.mx>
2014-08-20 18:01:33 -0500

    se elimina el modelo CourseGrade

--- app/models/course_grade.rb
+++
@@ -1,3 +0,0 @@
-class CourseGrade < ActiveRecord::Base
-  attr_accessible :course_id, :user_id
-end
--- app/models/grade.rb
+++ app/models/grade.rb
@@ -3,10 +3,6 @@ class Grade < ActiveRecord::Base
   belongs_to :user
   validates_associated :user
   validates :gradable, presence: true
-  validates :score, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
-
-  def score
-  	return "%.2g" % (read_attribute(:score) / 10.0)
-  end
+  validates :score, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
 
 end
