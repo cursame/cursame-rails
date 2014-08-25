4a18b6ae1b98ecb0b4381b7aeede63dd9b74d75e
Luis Alfredo <luis.alfredo@ciencias.unam.mx>
2014-08-25 11:54:07 -0500

    se actualiza dirección de correos

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,6 +70,6 @@ Cursame30Lb::Application.configure do
   config.action_mailer.delivery_method = :ses
   config.middleware.use ExceptionNotifier,
     :sender_address => %{"notifier" <error-notifier@cursa.me>},
-    :exception_recipients => %w{desarrollo@cursa.me}
+    :exception_recipients => %w{desarrollo+production@cursa.me}
 
 end
