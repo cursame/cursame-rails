156521cb0313708190d06bf2299884c8899c7cca
Luis Alfredo <luis.alfredo@ciencias.unam.mx>
2014-08-27 15:53:49 -0500

    fix en calificaciones de felipao

--- app/models/evaluation_criterium.rb
+++ app/models/evaluation_criterium.rb
@@ -3,6 +3,10 @@ class EvaluationCriterium < ActiveRecord::Base
 	has_many :members_in_course_criteria, dependent: :destroy
 	validates :name, presence: true
 
+  after_create do
+    self.evaluable.members_in_courses.each { |member| member.evaluate! } if self.evaluable_type == 'Course'
+  end
+
 	after_update do
 		self.evaluable.members_in_courses.each { |member| member.evaluate! } if self.evaluable_type == 'Course'
 	end
