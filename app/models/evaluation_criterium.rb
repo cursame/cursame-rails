440df30f40fefbd32772a9c55377a80334be8d53
Luis Alfredo <luis.alfredo@ciencias.unam.mx>
2014-08-27 16:36:54 -0500

    cambios al calcular promedio final

--- app/models/evaluation_criterium.rb
+++ app/models/evaluation_criterium.rb
@@ -4,10 +4,12 @@ class EvaluationCriterium < ActiveRecord::Base
 	validates :name, presence: true
 
   after_create do
+    puts "EvaluationCriterium : after_create"
     self.evaluable.members_in_courses.each { |member| member.evaluate! } if self.evaluable_type == 'Course'
   end
 
 	after_update do
+    puts "EvaluationCriterium : after_update"
 		self.evaluable.members_in_courses.each { |member| member.evaluate! } if self.evaluable_type == 'Course'
 	end
 
--- app/models/members_in_course.rb
+++ app/models/members_in_course.rb
@@ -226,20 +226,33 @@ class MembersInCourse < ActiveRecord::Base
   handle_asynchronously :import, :priority => 20, :run_at => Proc.new{Time.zone.now}
 
   # Generates the final grade in the course for this user, this method modifies this member in course.
+  # def evaluate!
+  #   puts "MembersInCourse : evaluate!"
+  #   if self.course.evaluation_criteria.blank? || needs_new_grade?
+  #     self.grade = Grade.new gradable: self, score: course_final_score, user: self.user
+  #   elsif grades_are_missing?
+  #     self.grade.destroy unless self.grade.nil?
+  #   else
+  #     self.grade = Grade.new gradable: self, score: course_final_score, user: self.user
+  #   end
+  #   self.save!
+  # end
+
   def evaluate!
-    if needs_new_grade? && (grades_are_missing? || self.course.evaluation_criteria.blank?)
-      self.grade = Grade.new gradable: self, score: course_final_score, user: self.user
-    else
+    puts "MembersInCourse : evaluate!"
+    if grades_are_missing?
       self.grade.destroy unless self.grade.nil?
+    else
+      self.grade = Grade.new gradable: self, score: course_final_score, user: self.user
     end
     self.save!
   end
 
-  #private
+  private
   # Returns true if grade for this member in course needs to be recalculated.
-  def needs_new_grade?
-    self.grade.nil? || self.grade.score != course_final_score
-  end
+  # def needs_new_grade?
+  #   self.grade.nil? || self.grade.score != course_final_score
+  # end
 
   def grades_are_missing?
     self.course.evaluation_criteria.inject(false) { |needs_grade,criteria| needs_grade || MembersInCourseCriterium.find_by_members_in_course_id_and_evaluation_criterium_id(self,criteria).nil? }
