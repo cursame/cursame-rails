0ab0b46ead65bd3a48f5dbd3cefca9d4e181d773
Salvador Mendo <simiographics@gmail.com>
2014-09-17 10:48:16 -0500

    Nuevo layout para seccion de managers

--- app/controllers/managers/courses_controller.rb
+++ app/controllers/managers/courses_controller.rb
@@ -4,73 +4,9 @@ class Managers::CoursesController < Managers::BaseController
   end
 
   def show
-    @course = Course.find_by_id params[:id]
+    @course = User.find_by_id params[:id]
   end
 
   def new
-    @course = Course.new
   end
-
-  def create
-    redirect_to managers_courses_path, flash: { success: 'Curso creado correctamente.' }
-  end
-
-  def edit
-    @course = Course.find_by_id params[:id]
-  end
-
-  def update
-    redirect_to managers_courses_path, flash: { success: 'Curso editado correctamente.' }
-  end
-
-  def destroy
-    redirect_to managers_courses_path, flash: { success: 'Curso borrado correctamente.' }
-  end
-
-  def import
-    @courses = current_network.courses
-  end
-
-  def import_receiver
-    network = current_network
-    user_admin = current_user
-
-    if Course.find_by_title_and_silabus("1","1").nil? then
-      course = Course.new(:title => "1", :silabus => "1")
-      course.save!
-    else
-      course = Course.find_by_title_and_silabus("1","1")
-    end
-
-    lastFile = Dir.glob("public/imports/import_courses_*").sort.last
-    if lastFile.nil? then
-      name = "import_courses_1.csv"
-    else
-      lastFile = lastFile.split("/").last
-      nameFile = lastFile[0...-4]
-      name = nameFile.succ + ".csv"
-    end
-
-    text = ""
-    begin
-      File.open(params[:file].path,'r').each do |line|
-        text += line
-      end
-
-
-      path = "public/imports/" + name
-      f = File.open(path,'w+')
-      f.write(text)
-      f.close
-
-      course.delay.import(path,network,user_admin)
-    rescue
-      @noFile = true
-    end
-
-    @courses = network.courses
-    
-    redirect_to import_managers_courses_path
-  end
-
 end
--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -12,10 +12,9 @@ authorization do
     has_permission_on [:surveys], :to => [:users,:create,:destroy,:index,:show,:edit]
     has_permission_on [:events], :to => [:users,:create,:destroy,:index,:show,:edit]
 
-    has_permission_on [:managers], :to => [ :index, :publications, :settings, :import_members, :upload_members, :mailer, :mailer_deliver ]
-    has_permission_on [:managers_users], :to => [ :index, :show, :new, :create, :edit, :update, :destroy, :import, :import_receiver ]
-    has_permission_on [:managers_courses], :to => [ :index, :show, :new, :create, :update, :edit, :destroy, :import, :import_receiver ]
-    has_permission_on [:managers_deliveries], :to => [ :index, :show, :new, :edit ]
+    has_permission_on [:managers], :to => [:settings, :create, :destroy, :index, :show, :edit, :wall, :members, :library, :import_courses, :upload_courses, :import_users, :upload_users, :import_members, :upload_members,:send_mails, :sending]
+    has_permission_on [:managers_users], :to => [:index, :show, :new, :import]
+    has_permission_on [:managers_courses], :to => [:index, :show, :new ]
   end
 
   role :student do
