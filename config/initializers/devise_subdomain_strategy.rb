c6b82ccf77425c9a46c6284facc293dfae7587b5
Manuel Lara <malh04@gmail.com>
2014-09-17 22:15:57 -0500

    Se agregó verificación de contraseña en la estrategia de subdominio

--- config/initializers/devise_subdomain_strategy.rb
+++ config/initializers/devise_subdomain_strategy.rb
@@ -10,16 +10,18 @@ module Devise
 
       def authenticate!
         user = User.find_by_email(params['user']['email'])
-        unless user.blank?
+
+        if user && user.valid_password?(params['user']['password'])
           if user.permissionings.first.role_id == 4
             success!(user)
           else
             subdomain = user.permissionings.first.network.subdomain
-            subdomain == request.subdomain.downcase ? success!(user) : fail!("No pertenece a esta red")
+            subdomain == request.subdomain.downcase ? success!(user) : fail!
           end 
         else
-          fail!("No existe el usuario")
+          fail!
         end
+
       end
     end
   end
