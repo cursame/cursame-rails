a7a3155c258abe407c0ecf7d5ac38f676297f430
Salvador Mendo <simiographics@gmail.com>
2014-09-19 16:27:47 -0500

    Agregar boton borrar manager cursos

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -11,9 +11,9 @@ authorization do
     has_permission_on [:discussions], :to => [:users,:create,:destroy,:index,:show,:edit]
     has_permission_on [:surveys], :to => [:users,:create,:destroy,:index,:show,:edit]
     has_permission_on [:events], :to => [:users,:create,:destroy,:index,:show,:edit]
-    has_permission_on [:managers], :to => [:settings, :create, :destroy, :index, :show, :edit, :wall, :members, :library, :import_courses, :upload_courses, :import_users, :upload_users, :import_members, :upload_members, :mailer, :mailer_deliver]
-    has_permission_on [:managers_users], :to => [:index, :show, :new, :create, :edit, :update, :destroy, :import, :import_receiver]
-    has_permission_on [:managers_courses], :to => [:index, :show, :new, :create, :update, :edit, :import, :import_receiver ]
+    has_permission_on [:managers], :to => [ :index, :settings, :import_members, :upload_members, :mailer, :mailer_deliver ]
+    has_permission_on [:managers_users], :to => [ :index, :show, :new, :create, :edit, :update, :destroy, :import, :import_receiver ]
+    has_permission_on [:managers_courses], :to => [ :index, :show, :new, :create, :update, :edit, :destroy, :import, :import_receiver ]
   end
 
   role :student do
