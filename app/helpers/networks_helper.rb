dcf6389edc2d1fcd0ee221baeb93e11cec87ae32
Salvador Mendo <simiographics@gmail.com>
2014-09-23 16:48:58 -0500

    Network utils for users

--- app/helpers/networks_helper.rb
+++ app/helpers/networks_helper.rb
@@ -1,13 +1,11 @@
 module NetworksHelper
-  def vlex_remote_auth
+  include NetworksUtils
 
-    params = { 
-      account_id: current_network.find_setting(:vlex_account_id).value, 
-      name: current_network.find_setting(:vlex_name).value, 
-      token: current_network.find_setting(:vlex_token).value, 
-      email: current_network.find_setting(:vlex_email).value
-    }
-    
+  # Example:
+  # data = {:account_id => "1234", :name => "tester", :token => "TOKEN", :email => "test@vlex.com"}
+  # puts vlex_remote_auth data
+  def vlex_remote_auth(params)
+    vlex_remote_auth_url = "http://vlex.com/session/remote_auth"
     # Add timestamp
     params = params.clone # so that the delete at the end doesn't update the params hash
     params[:timestamp] = Time.now.to_i
@@ -26,5 +24,17 @@ module NetworksHelper
     
     # Generate the query and return the URL
     "#{vlex_remote_auth_url}?#{params.to_query}"
+  end
+
+  def get_network_students
+    return network_students
+  end
+
+  def get_network_teachers
+    return network_teachers
+  end
+
+  def get_network_managers
+    return network_managers
   end
 end
