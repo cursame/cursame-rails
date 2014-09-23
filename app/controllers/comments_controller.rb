a819eeb6a8de2e646cc576804aa8c5dd59e3ee0d
lruiz05 <luis05021990@gmail.com>
2014-09-23 15:54:05 -0500

    fix conflict app/views/shared/_notification_type.html.erb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,9 +8,17 @@ class CommentsController < ApplicationController
     @wall = Wall.find_by_publication_type_and_publication_id( 'Comment', @comment.id )
   end
 
+  # def validations
+  #   @comment = Comment.find_by_id(params[:id]) 
+  #   redirect_to root_path, flash: { error: "El comentario que intentas ver no existe o ha sido borrado."} and return if @comment.nil?
+  #   course_member?(current_user, @comment.commentable)
+  # end
+
   def validations
     @comment = Comment.find_by_id(params[:id]) 
     redirect_to root_path, flash: { error: "El comentario que intentas ver no existe o ha sido borrado."} and return if @comment.nil?
-    course_member?(current_user, @comment.commentable)
+    if @comment.commentable == Course
+      course_member?(current_user, @comment.commentable)
+    end
   end
 end
