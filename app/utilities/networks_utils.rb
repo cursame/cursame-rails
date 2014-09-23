28f5b72f26b2358e04fa9c6caf1409ea6d41b7bd
lruiz05 <luis05021990@gmail.com>
2014-09-23 18:07:08 -0500

    se agrega flag a metodos de networks_utils

--- app/utilities/networks_utils.rb
+++ app/utilities/networks_utils.rb
@@ -1,16 +1,28 @@
 # coding: utf-8
 module NetworksUtils
 
-  def network_students
-    current_network.users.keep_if {|user| user.student?}
+  def network_students(flag)
+  	if flag
+    	current_network.users.keep_if {|user| user.student?}
+    else
+    	current_network.users.keep_if {|user| user.student? and current_user != user}
+    end
   end
 
-  def network_teachers
-  	current_network.users.keep_if {|user| user.teacher?}
+  def network_teachers(flag)
+  	if flag
+  		current_network.users.keep_if {|user| user.teacher?}
+  	else
+  		current_network.users.keep_if {|user| user.teacher? and current_user != user}
+  	end
   end
 
-  def network_managers
-    current_network.users.keep_if {|user| user.admin?}
+  def network_managers(flag)
+  	if flag
+    	current_network.users.keep_if {|user| user.admin?}
+    else
+    	current_network.users.keep_if {|user| user.admin? and current_user != user}
+    end
   end
 
 end
