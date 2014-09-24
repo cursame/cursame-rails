9e4aa0bd7debb3c742108e453d1c81020b4b7fa4
Salvador Mendo <simiographics@gmail.com>
2014-09-24 13:30:14 -0500

    Deliveries en managers

--- app/controllers/managers/deliveries_controller.rb
+++ app/controllers/managers/deliveries_controller.rb
@@ -3,7 +3,27 @@ class Managers::DeliveriesController < Managers::BaseController
     @deliveries = Delivery.all
   end
 
+  def show
+    @delivery = Delivery.find_by_id params[:id]
+  end
+
   def new
     @delivery = Delivery.new
+  end
+
+  def create
+    redirect_to managers_deliveries_path, flash: { success: 'Tarea actualizada correctamente.' }
+  end
+
+  def edit
+    @delivery = Delivery.find_by_id params[:id]
+  end
+
+  def update
+    redirect_to managers_deliveries_path, flash: { success: 'Tarea actualizada correctamente.' }
+  end
+
+  def destroy
+    redirect_to managers_deliveries_path, flash: { success: 'Tarea borrada correctamente.' }
   end
 end
