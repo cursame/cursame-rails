ad4878543883afe029ce75eda5921f795a71ccef
Salvador Mendo <simiographics@gmail.com>
2014-09-29 13:41:13 -0500

    Discussions en managers

---
+++ app/controllers/managers/discussions_controller.rb
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+class Managers::DiscussionsController < Managers::BaseController
+  def index
+    @discussions = Discussion.all
+  end
+
+  def show
+    @discussion = Discussion.find_by_id params[:id]
+  end
+
+  def destroy
+    redirect_to managers_discussions_path, flash: { success: 'Discusión borrada correctamente.' }
+  end
+end
