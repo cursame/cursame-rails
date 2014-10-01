5e18d5772ba626b705fe06fdd5a351488e0775c2
Luis Alfredo <luis.alfredo@ciencias.unam.mx>
2014-10-01 18:02:26 -0500

    se agregan validaciones sobre destroy

--- app/controllers/managers/discussions_controller.rb
+++ app/controllers/managers/discussions_controller.rb
@@ -1,18 +1,16 @@
 # -*- coding: utf-8 -*-
-
 class Managers::DiscussionsController < Managers::BaseController
   def index
     @discussions = Discussion.all
   end
 
   def show
-    @discussion = Discussion.find_by_id params[:id]
+    @discussion = Discussion.find_by_id(params[:id])
   end
 
   def destroy
-    @discussion = Discussion.find params[:id]
-    @discussion.destroy
-    
+    discussion = Discussion.find_by_id(params[:id])
+    discussion.destroy
     redirect_to managers_discussions_path, flash: { success: 'Discusión borrada correctamente.' }
   end
 end
