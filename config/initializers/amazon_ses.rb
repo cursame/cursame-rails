a4e8c1200e14f500d326a330fda3429cea801cbe
Luis Alfredo <luis.alfredo@ciencias.unam.mx>
2014-10-10 13:01:36 -0500

    cambio en las credenciales de AWS

--- config/initializers/amazon_ses.rb
+++ config/initializers/amazon_ses.rb
@@ -1,3 +1,3 @@
 ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
-  :access_key_id     => 'AKIAISIGOKLLQ6T5H3NQ',
-  :secret_access_key => '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'
+  :access_key_id     => 'AKIAIDEOIKQ6IJYTQ24A',
+  :secret_access_key => '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'
