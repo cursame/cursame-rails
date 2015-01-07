839025b6af38312b8cd18cf9e040cbb4fb06e86c
Isaac Pompa <isaacsalvador18@gmail.com>
2015-01-07 13:08:56 -0600

    Simplifico la consulta del token

--- app/workers/mixpanel_tracker_worker.rb
+++ app/workers/mixpanel_tracker_worker.rb
@@ -10,9 +10,7 @@ class MixpanelTrackerWorker
   # A call to track is a report that an event has occurred
   # http://mixpanel.github.io/mixpanel-ruby/Mixpanel/Tracker.html#method-i-track
   def perform(distinct_id, event, properties={})
-    puts 'Entro a perform'
-    network_id = Network.find_by_subdomain(properties['Subdomain']).id
-    tracker = mixpanel(NetworkSetting.find_by_network_id(network_id).value) #aqui pasa el token guardado en la tabla NetworkSetting
+    tracker = mixpanel(Network.find_by_subdomain(properties['Subdomain']).network_settings[0].value) #aqui pasa el token guardado en la tabla NetworkSetting
     tracker.track distinct_id, event, properties
   end
 
