04581284added9fb02ee9ce42fff697e6d721eeb
Luis Alfredo <luis.alfredo@ciencias.unam.mx>
2015-01-12 10:34:44 -0600

    adding cucumber configuration

--- features/support/env.rb
+++ features/support/env.rb
@@ -31,9 +31,16 @@ ActionController::Base.allow_rescue = false
 # Remove/comment out the lines below if your app doesn't have a database.
 # For some databases (like MongoDB and CouchDB) you may need to use :truncation instead.
 begin
-  DatabaseCleaner.strategy = :transaction
+  require 'database_cleaner'
+  require 'database_cleaner/cucumber'
+  
+  DatabaseCleaner.strategy = :truncation
 rescue NameError
   raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
+end
+
+Around do |scenario, block|
+  DatabaseCleaner.cleaning(&block)
 end
 
 # You may also want to configure DatabaseCleaner to use different strategies for certain features and scenarios.
