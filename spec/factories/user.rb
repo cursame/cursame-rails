7b9f4574d244bfaf4603de0176277a032e01f9da
Luis Alfredo <luis.alfredo@ciencias.unam.mx>
2015-01-19 15:49:10 -0600

    adding factories for users and permissionings

---
+++ spec/factories/user.rb
@@ -0,0 +1,18 @@
+FactoryGirl.define do
+  factory :user do
+    email 'desarrollo+factory@cursa.me'
+    password 'cursameFactory'
+    first_name 'Test'
+    last_name 'User'
+    confirmed_at Time.now
+    personal_url 'test-user'
+    domain 'cursa.me'
+    subdomain { FactoryGirl.create(:network).subdomain }
+
+    factory :user_with_student_role do
+      after(:build) do |user| 
+        user.permissionings << FactoryGirl.create(:permissioning_with_student_role, user: user, network: Network.last)
+      end
+    end
+  end
+end
