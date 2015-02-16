fdd32ab1f35e38d073abfc86d6ad62eaab055bfa
Luis Alfredo <luis.alfredo@ciencias.unam.mx>
2015-02-16 12:49:39 -0600

    adding init and term date

--- app/models/wufoo_form.rb
+++ app/models/wufoo_form.rb
@@ -2,5 +2,5 @@ class WufooForm < ActiveRecord::Base
   has_many :wufoo_responses, dependent: :destroy
   has_many :wufoo_form_roles, dependent: :destroy
   belongs_to :showable, polymorphic: true
-  attr_accessible :form_id, :showable
+  attr_accessible :form_id, :showable, :init_date, :term_date
 end
--- db/migrate/20150205181038_create_wufoo_forms.rb
+++ db/migrate/20150205181038_create_wufoo_forms.rb
@@ -3,6 +3,8 @@ class CreateWufooForms < ActiveRecord::Migration
     create_table :wufoo_forms do |t|
       t.integer :form_id
       t.references :showable, polymorphic: true, index: true
+      t.datetime :init_date
+      t.datetime :term_date
 
       t.timestamps
     end
