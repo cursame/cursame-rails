cabd75670efd85161660a27caf21816a5566735f
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2015-03-20 16:44:09 -0600

    beautifying code

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,41 +1,35 @@
 # -*- coding: utf-8 -*-
 class Network < ActiveRecord::Base
-  has_one :network_template#, :dependent => :destroy
+
+  has_one :network_template
   has_one :wufoo_setting, dependent: :destroy
 
-  has_many :permissionings, :dependent => :destroy
-  has_many :users, :through => :permissionings
-  has_many :discussions, :dependent => :destroy
-  has_many :deliveries#, :dependent => :destroy
-  has_many :courses, :dependent => :destroy
-  has_many :comments, :dependent => :destroy
-  has_many :surveys#, :dependent => :destroy
-  #walls
-  has_many :walls, :dependent => :destroy
-  #library
-  has_many :libraries#, :dependent => :destroy
+  has_many :permissionings, dependent: :destroy
+  has_many :users, through: :permissionings
+  has_many :discussions, dependent: :destroy
+  has_many :deliveries
+  has_many :courses, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :surveys
+  has_many :walls, dependent: :destroy
+  has_many :libraries
   has_many :network_settings
   has_many :wufoo_forms, as: :showable, dependent: :destroy
   has_many :reported_contents, dependent: :destroy
 
   validates_presence_of   :name, :subdomain, :population
   validates_uniqueness_of :subdomain
-  validates_format_of     :subdomain, :with => /^[\-a-z0-9]+$/
+  validates_format_of     :subdomain, with: /^[\-a-z0-9]+$/
 
-
- 
-  #comentarios para las redes
   acts_as_commentable
 
   accepts_nested_attributes_for :users
   accepts_nested_attributes_for :permissionings
   accepts_nested_attributes_for :wufoo_setting
 
-  #uploader de imagenes de fondo de redes
-  
   mount_uploader :image_front, BackendFromNetworkUploader
   mount_uploader :logo, LogoNetworkUploader
-  
+
   after_create do
     gospel_add_network
   end
@@ -57,19 +51,14 @@ class Network < ActiveRecord::Base
   end
 
   def publications (user_id, network_id)
-    Wall.scoped(:include =>{
-          :users => :userpublicationings,
-          :courses => :coursepublicationings,
-          :courses => :members_in_courses
-    },
-    :conditions => ["(walls.network_id = ?) AND
+    Wall.scoped(include: {
+                  users: :userpublicationings,
+                  courses: :coursepublicationings,
+                  courses: :members_in_courses
+                },
+                conditions: ["(walls.network_id = ?) AND
        (userpublicationings.user_id = ? OR walls.public = ?) OR
       (members_in_courses.accepted = ? AND members_in_courses.user_id = ? AND walls.publication_type != 'Comment') ",network_id,user_id,true,true, user_id]).order('walls.created_at DESC')
-      # :conditions => ["(walls.network_id = ?) AND
-      #                 ((walls.public = ? AND walls.publication_type != 'Comment') OR (userpublicationings.user_id = ? AND walls.publication_type != 'Comment')) OR
-      #                 (members_in_courses.accepted = ? AND members_in_courses.user_id = ? AND walls.publication_type != 'Comment')",network_id,true,user_id,true, user_id]).order('walls.created_at DESC')
-
-
   end
 
 
@@ -85,7 +74,7 @@ class Network < ActiveRecord::Base
     end
   end
 
-  handle_asynchronously :send_email, :priority => 20, :run_at => Proc.new{Time.zone.now}
+  handle_asynchronously :send_email, priority: 20, run_at: Proc.new{Time.zone.now}
 
   def averageCalificationSurvey
     surveys = self.surveys
@@ -115,7 +104,7 @@ class Network < ActiveRecord::Base
     end
     return average/deliveries.size
   end
-  
+
   def self.averageNetworkSurvey
     networks = Network.all
     if (networks.size == 0) then
@@ -141,52 +130,50 @@ class Network < ActiveRecord::Base
 
     return average/networks.size
   end
-  
-  ##### definiciones de cambios de etiquetas especializadas.
-  
+
   def user_tag
     @defter = (self.titles).to_s.split(',')[0]
-    @self_defter = @defter.split(':').last   
+    @self_defter = @defter.split(':').last
   end
-  
+
   def profesor_tag
     @defter = (self.titles).to_s.split(',')[1]
     @self_defter = @defter.split(':').last
   end
-  
+
   def student_tag
     @defter = (self.titles).to_s.split(',')[2]
     @self_defter = @defter.split(':').last
   end
-  
+
   def admin_tag
     @defter = (self.titles).to_s.split(',')[3]
     @self_defter = @defter.split(':').last
   end
-  
+
   def course_tag
-     @defter = (self.titles).to_s.split(',')[4]
-     @self_defter = @defter.split(':').last
+    @defter = (self.titles).to_s.split(',')[4]
+    @self_defter = @defter.split(':').last
   end
-  
+
   def courses_tag
-     @defter = (self.titles).to_s.split(',')[5]
-     @self_defter = @defter.split(':').last
+    @defter = (self.titles).to_s.split(',')[5]
+    @self_defter = @defter.split(':').last
   end
-  
+
   def friend_tag
-     @defter = (self.titles).to_s.split(',')[6]
-     @self_defter = @defter.split(':').last
+    @defter = (self.titles).to_s.split(',')[6]
+    @self_defter = @defter.split(':').last
   end
-  
+
   def friends_tag
-     @defter = (self.titles).to_s.split(',')[7]
-     @self_defter = @defter.split(':').last
+    @defter = (self.titles).to_s.split(',')[7]
+    @self_defter = @defter.split(':').last
   end
-  
+
   def comunity_tag
-      @defter = (self.titles).to_s.split(',')[8]
-      @self_defter = @defter.split(':').last
+    @defter = (self.titles).to_s.split(',')[8]
+    @self_defter = @defter.split(':').last
   end
 
   def mixpanel_token?
