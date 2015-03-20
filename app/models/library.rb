dca3a4e578dee9dd7ead56f1610b756e65a57a24
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2015-03-20 17:57:14 -0600

    Fix on class name

--- app/models/library.rb
+++ app/models/library.rb
@@ -1,5 +1,5 @@
 class Library < ActiveRecord::Base
   belongs_to :storable, polymorphic: true
   belongs_to :network
-  has_many :files, class_name: 'LibraryFiles', foreign_key: 'library_id', dependent: :destroy
+  has_many :files, class_name: 'LibraryFile', foreign_key: 'library_id', dependent: :destroy
 end
