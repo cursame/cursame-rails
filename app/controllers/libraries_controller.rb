a672539c57f06b32b8a45101d2c6bf57aee8e5dc
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2015-03-23 19:25:17 -0600

    Adding libraries#show

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -3,4 +3,9 @@ class LibrariesController < ApplicationController
     @libraries = current_network.libraries.reject { |library| library.storable.kind_of? Network }
     @files = current_network.files
   end
+
+  def show
+    library = Library.find params[:id]
+    @files = library.files
+  end
 end
