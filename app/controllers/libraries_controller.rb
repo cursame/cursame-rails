a57eb28145fcca4c315513c6801d9dcd6a2919a1
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2015-03-23 18:33:14 -0600

    Adding libraries index views

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -1,6 +1,6 @@
 class LibrariesController < ApplicationController
   def index
-    @libraries = current_network.libraries
+    @libraries = current_network.libraries.reject { |library| library.storable.kind_of? Network }
     @files = current_network.files
   end
 end
