d39aaf1be048e4eace6e641335692a0bb9ef7cac
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2015-03-25 15:32:18 -0600

    Removing index from libraries controller

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -1,9 +1,4 @@
 class LibrariesController < ApplicationController
-  def index
-    @libraries = current_network.libraries.reject { |library| library.storable.kind_of? Network }
-    @files = current_network.library_files
-  end
-
   def show
     @library = Library.find params[:id]
     @files = @library.library_files
