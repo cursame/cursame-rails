f7a6aab6cd8fdf110570bffba06015678003efe2
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2015-03-25 20:08:59 -0600

    Removing walls for library file

--- app/models/library_directory.rb
+++ app/models/library_directory.rb
@@ -1,8 +1,8 @@
 class LibraryDirectory < ActiveRecord::Base
   has_many :library_directories, as: :location, dependent: :destroy
   has_many :library_files, as: :location, dependent: :destroy
-
   belongs_to :location, polymorphic: true
+  
   attr_accessible :name, :description
 
   def library
--- app/models/library_file.rb
+++ app/models/library_file.rb
@@ -1,15 +1,10 @@
 class LibraryFile < ActiveRecord::Base
-  has_one :wall, as: :publication, dependent: :destroy
-  
   belongs_to :location, polymorphic: true
   belongs_to :user
 
   mount_uploader :file, LibraryFileUploader
-  acts_as_commentable
-  acts_as_votable
 
   after_create do
-    create_wall
     create_notification
   end
 
@@ -23,17 +18,8 @@ class LibraryFile < ActiveRecord::Base
   end
 
   private
-  def create_wall
-    case location.storable
-    when Network, Library
-      create_wall_for_network
-    when Course
-      create_wall_for_course
-    end
-  end
-
   def create_notification
-    case location.storable
+    case location
     when Network, Library
       create_notification_for_network
     when Course
@@ -41,19 +27,11 @@ class LibraryFile < ActiveRecord::Base
     end
   end
 
-  def create_wall_for_network
-    Wall.create publication: self, network: location.network, users: location.network.users
-  end
-
-  def create_wall_for_course
-    Wall.create publication: self, network: location.network, courses: [ location.storable ], users: location.storable.users
-  end
-
   def create_notification_for_course
-    Notification.create users: location.storable.users, notificator: self, kind: 'new_library_file'
+    Notification.create users: library.storable.users, notificator: self, kind: 'new_library_file'
   end
 
   def create_notification_for_network
-    Notification.create users: location.network.users, notificator: self, kind: 'new_library_file'
+    Notification.create users: library.network.users, notificator: self, kind: 'new_library_file'
   end
 end
