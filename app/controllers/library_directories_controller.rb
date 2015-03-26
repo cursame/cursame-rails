5a483a7c3e9cabfcfafe5583019dc10b7e4ec903
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2015-03-25 20:07:52 -0600

    adding actions to directories and files controller

--- app/controllers/library_directories_controller.rb
+++ app/controllers/library_directories_controller.rb
@@ -1,13 +1,46 @@
 class LibraryDirectoriesController < ApplicationController
+  def show
+    @directory = LibraryDirectory.find(params[:id])
+  end
+
   def new
-    @library = Library.find(params[:library_id])
-    @directory = @library.library_directories.build
+    if params[:library_id]
+      new_directory_for_library
+    elsif params[:library_directory_id]
+      new_directory_for_directory
+    end
   end
 
   def create
+    if params[:library_id]
+      create_directory_for_library
+    elsif params[:library_directory_id]
+      create_directory_for_directory
+    end
+  end
+
+  private
+  def new_directory_for_library
+    @parent_model = Library.find(params[:library_id])
+    @directory = @parent_model.library_directories.build
+  end
+
+  def new_directory_for_directory
+    @parent_model = LibraryDirectory.find(params[:library_directory_id])
+    @directory = @parent_model.library_directories.build
+  end
+
+  def create_directory_for_library
     library = Library.find(params[:library_id])
     directory = library.library_directories.build(params[:library_directory])
     directory.save
     redirect_to library_path(library)
+  end
+
+  def create_directory_for_directory
+    parent_directory = LibraryDirectory.find(params[:library_directory_id])
+    directory = parent_directory.library_directories.build(params[:library_directory])
+    directory.save
+    redirect_to library_directory_path(parent_directory)
   end
 end
--- app/controllers/library_files_controller.rb
+++ app/controllers/library_files_controller.rb
@@ -1,8 +1,44 @@
 class LibraryFilesController < ApplicationController
-  def create
-    LibraryFile.create params[:library_file] do |file|
-      file.user = current_user
+  def new
+    if params[:library_id]
+      new_file_for_library
+    elsif params[:library_directory_id]
+      new_file_for_directory
     end
-    redirect_to root_path
+  end
+
+  def create
+    if params[:library_id]
+      create_file_for_library
+    elsif params[:library_directory_id]
+      create_file_for_directory
+    end
+  end
+
+  private
+  def new_file_for_library
+    @parent_model = Library.find(params[:library_id])
+    @file = @parent_model.library_files.build
+  end
+
+  def new_file_for_directory
+    @parent_model = LibraryDirectory.find(params[:library_directory_id])
+    @file = @parent_model.library_files.build
+  end
+
+  def create_file_for_library
+    library = Library.find(params[:library_id])
+    file = library.library_files.build(params[:library_file])
+    file.user = current_user
+    file.save
+    redirect_to library_path(library)
+  end
+
+  def create_file_for_directory
+    directory = LibraryDirectory.find(params[:library_directory_id])
+    file = directory.library_files.build(params[:library_file])
+    file.user = current_user
+    file.save
+    redirect_to library_directory_path(directory)
   end
 end
