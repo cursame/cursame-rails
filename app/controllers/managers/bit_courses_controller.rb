eecbeadd7a3cdcdc44d908966d6c7546f7828537
lruiz05 <luis05021990@gmail.com>
2015-04-28 16:26:05 -0500

    create controller bit_courses

---
+++ app/controllers/managers/bit_courses_controller.rb
@@ -0,0 +1,103 @@
+class Managers::BitCoursesController < Managers::BaseController
+  def index
+    unless bit_courses.nil?
+      @bit_courses = bit_courses
+    else
+      redirect_to managers_courses_path, flash: { error: 'Ocurrio un error, no se pudo acceder a los grupos de Bit.'}
+    end
+  end
+
+  def show
+    folio = params[:folio]
+    @course = bit_course(folio)
+    @students = bit_students(folio)
+    @teachers = bit_teachers(folio)
+  end
+  
+  def import
+    folio = params[:folio]
+    group_students = bit_students(folio)
+    group_teachers = bit_teachers(folio)
+    group = bit_course(folio)
+
+    unless group_teachers.nil? or group_students.nil? or group.nil?
+      @course_cursame = Course.new(title: group['asignatura'], silabus: group['nombreCompletoGrupo'], init_date: group['fechaInicialCiclo'].to_datetime, public_status: 'Private', network_id: current_network.id)
+      cursame_students = cursame_students(group_students)
+      cursame_teachers = cursame_teachers(group_teachers)
+      @course_cursame.members_in_courses = @course_cursame.add_teachers(cursame_teachers) + @course_cursame.add_students(cursame_students)
+      if @course_cursame.save
+        success_link = link_course_to_group(@course_cursame.id, folio)
+        if success_link
+          the_flash = { success: 'Grupo importado correctamente, Se ha creado el curso.' }
+        else
+          the_flash = { error: 'Ocurrio un error, no se pudo enlazar el curso con Bit.'}
+          @course_cursame.destroy
+        end
+      else
+        the_flash = { error: 'Ocurrio un error al crear el curso.' }
+      end
+    else
+      the_flash = { error: 'Ocurrio un error al importar los datos del curso'}
+    end
+    redirect_to managers_courses_path, flash: the_flash
+  end
+
+  private
+
+  def bit_courses
+    begin
+      response = HTTParty.get('http://api.academic.mx:80/v1/grupos', headers: { "Authorization" => BIT_API_KEY}, timeout: 180)
+      courses = response.code == 200 ? response : nil
+    rescue Exception => e
+      puts e.message
+    end
+  end
+
+  def bit_students(folio)
+    url = 'http://api.academic.mx:80/v1/alumnos/' + folio
+    begin
+      response = HTTParty.get(url, headers: {"Authorization" => BIT_API_KEY}, timeout: 180)
+      students = response.code == 200 ? response : nil
+    rescue Exception => e
+      puts e.message
+    end
+  end
+
+  def bit_teachers(folio)
+    url = 'http://api.academic.mx:80/v1/profesores/' + folio
+    begin
+      response = HTTParty.get(url, headers: {"Authorization" => BIT_API_KEY}, timeout: 180)
+      teachers = response.code == 200 ? response : nil
+    rescue Exception => e
+      puts e.message
+    end
+  end
+
+  def link_course_to_group(id, folio)
+    url = "http://api.academic.mx:80/v1/grupos"
+    begin
+      response = HTTParty.post(url, headers: { "Authorization" => BIT_API_KEY}, body: {"grupos" => [{'grupo' => folio, 'idExterno' => id}]}, timeout: 180)
+      success = response.code == 200
+    rescue Exception => e
+      puts e.message
+      false
+    end
+  end
+
+  def bit_course(folio)
+    if bit_courses.nil?
+      bit_courses
+    else
+      bit_courses.select{|course| course['folio'] == params[:folio]}.first
+    end
+  end
+
+  def cursame_students(bit_students)
+    bit_students.map{|bit_student| User.find_by_email bit_student['correo']}
+  end
+
+  def cursame_teachers(bit_teachers)
+    bit_teachers.map{|bit_student| User.find_by_email bit_student['SCorreo']}
+  end
+
+end
