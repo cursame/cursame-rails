2006a965bfea7bfff5d17b3b2276ca35e5a57e5b
alfredo777 <jose_alfredo@cursa.me>
2015-05-12 16:25:19 -0500

    cambios en helper

--- app/helpers/surveys_helper.rb
+++ app/helpers/surveys_helper.rb
@@ -1,9 +1,11 @@
 module SurveysHelper
 	def survey_notice(timer)
-		if timer.to_i == 0
-			false
-		else
-			t('.notice_helper') + " #{timer} " +  t('.notice_helper2')
-		end
+    if current_user.student?
+  		if timer.to_i == 0
+  			false
+  		else
+  			t('.notice_helper') + " #{timer} " +  t('.notice_helper2')
+  		end
+    end
 	end
 end
