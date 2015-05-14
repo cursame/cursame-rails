9e97a4f2bc92f05f913ac6398d02ea474326e3df
lruiz05 <luis05021990@gmail.com>
2015-05-14 18:08:50 -0500

    al importar grupo de bit, se crean usuarios del curso si no existen

--- app/controllers/managers/bit_courses_controller.rb
+++ app/controllers/managers/bit_courses_controller.rb
@@ -1,7 +1,7 @@
 class Managers::BitCoursesController < Managers::BaseController
   rescue_from Timeout::Error, with: :error_connection
   rescue_from Errors::ErrorResponseAppBit, with: :error_connection
-  rescue_from ActiveRecord::RecordInvalid, with: :error_creating_course
+  rescue_from ActiveRecord::RecordInvalid, with: :error_create
 
   def index
     @bit_courses = bit_courses
@@ -15,12 +15,13 @@ class Managers::BitCoursesController < Managers::BaseController
 
   def import
     group = bit_course
-    @course = create_course(group)
+    @course = new_course(group)
     students = cursame_students(bit_students)
     teachers = cursame_teachers(bit_teachers)
     @course.members_in_courses =
       @course.add_teachers(teachers) + @course.add_students(students)
-    link_course_to_group(@course.id, params[:folio]) if @course.save!
+    @course.save!
+    link_course_to_group(@course.id, params[:folio])
     inf_flash = { success: t('.managers.bit_messages.success_importing_group') }
     redirect_to index_managers_bit_courses_path, flash: inf_flash
   end
@@ -32,8 +33,8 @@ class Managers::BitCoursesController < Managers::BaseController
     redirect_to :back, flash: info_flash
   end
 
-  def error_creating_course
-    info_flash = { error: t('.managers.bit_messages.error_creating_course') }
+  def error_create
+    info_flash = { error: 'Error, no se pudo crear el objeto' }
     redirect_to :back, flash: info_flash
   end
 
@@ -86,7 +87,8 @@ class Managers::BitCoursesController < Managers::BaseController
   end
 
   def bit_students
-    uri = build_uri_group_students + params[:folio]
+    uri = build_uri_group_students
+    uri.path = uri.path + '/' + params[:folio]
     response = HTTParty.get(
       uri,
       headers: { 'Authorization' => authorization },
@@ -96,7 +98,8 @@ class Managers::BitCoursesController < Managers::BaseController
   end
 
   def bit_teachers
-    uri = build_uri_group_teachers + params[:folio]
+    uri = build_uri_group_teachers
+    uri.path = uri.path + '/' + params[:folio]
     response = HTTParty.get(
       uri,
       headers: { 'Authorization' => authorization },
@@ -116,7 +119,7 @@ class Managers::BitCoursesController < Managers::BaseController
     response.code == 200 ? response : raise_error_response(uri, response, mess)
   end
 
-  def create_course(group)
+  def new_course(group)
     Course.new(
       title: group['asignatura'],
       silabus: group['nombreCompletoGrupo'],
@@ -126,15 +129,101 @@ class Managers::BitCoursesController < Managers::BaseController
     )
   end
 
+  def new_user_student(bit_user)
+    last_name = bit_user['apellidoPaterno'] + ' ' + bit_user['apellidoMaterno']
+    User.new(
+      email: bit_user['correo'],
+      password: 'cursame_bit',
+      first_name: bit_user['nombre'],
+      last_name: last_name,
+      personal_url: SecureRandom.uuid,
+      subdomain: current_network.subdomain,
+      domain: request.domain
+    )
+  end
+
+  def new_user_teacher(bit_user)
+    last_name = bit_user['apellidoPaterno'] + ' ' + bit_user['apellidoMaterno']
+    User.new(
+      email: bit_user['sCorreo'],
+      password: 'cursame_bit',
+      first_name: bit_user['nombre'],
+      last_name: last_name,
+      personal_url: SecureRandom.uuid,
+      subdomain: current_network.subdomain,
+      domain: request.domain
+    )
+  end
+
+  def new_permissioning_teacher(id)
+    Permissioning.new(
+      user_id:  id,
+      role_id: '3',
+      network_id: current_network.id
+    )
+  end
+
+  def new_permissioning_student(id)
+    Permissioning.new(
+      user_id:  id,
+      role_id: '2',
+      network_id: current_network.id
+    )
+  end
+
+  def create_student(bit_user)
+    user = new_user_student(bit_user)
+    user.permissionings = [new_permissioning_student(user.id)]
+    user.save!
+    link_student(user.id, bit_user['idAlumno'])
+    user
+  end
+
+  def create_teacher(bit_user)
+    user = new_user_teacher(bit_user)
+    user.permissionings = [new_permissioning_teacher(user.id)]
+    user.save!
+    link_teacher(user.id, bit_user['idProfesor'])
+    user
+  end
+
+  def link_student(id, bit_id)
+    uri = build_uri_group_students
+    response = HTTParty.post(
+      uri,
+      headers: { 'Authorization' => authorization },
+      body: { 'alumnos' => [{ 'idAlumno' => bit_id, 'idExterno' => id }] },
+      timeout: 180)
+    mess = 'Error linking cursame user with bit user student: '
+    raise_error_response(uri, response, mess) unless response.code == 200
+  end
+
+  def link_teacher(id, bit_id)
+    uri = build_uri_group_teachers
+    response = HTTParty.post(
+      uri,
+      headers: { 'Authorization' => authorization },
+      body: { 'profesores' => [{ 'idProfesor' => bit_id, 'idExterno' => id }] },
+      timeout: 180)
+    mess = 'Error linking cursame user with bit user teacher: '
+    raise_error_response(uri, response, mess) unless response.code == 200
+  end
+
   def bit_course
     bit_courses.select { |course| course['folio'] == params[:folio] }.first
   end
 
   def cursame_students(bit_students)
-    bit_students.map { |bit_student| User.find_by_email bit_student['correo'] }
+    bit_students.map do |bit_student|
+      user = User.find_by_email bit_student['correo']
+      user.nil? ? create_student(bit_student) : user
+    end
   end
 
   def cursame_teachers(bit_teachers)
-    bit_teachers.map { |bit_student| User.find_by_email bit_student['SCorreo'] }
+    bit_teachers.map do |bit_teacher|
+      user = User.find_by_email bit_teacher['sCorreo']
+      user.nil? ? create_teacher(bit_teacher) : user
+    end
   end
 end
