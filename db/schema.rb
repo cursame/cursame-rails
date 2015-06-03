a978f1950c009321af0346db2fdda14c0f3dfd04
Luis Alfredo Lorenzo <luis.alfredo@ciencias.unam.mx>
2015-06-03 18:50:52 -0500

    Removes free column from Networks table
    Adds a rails migration to remove the column and removes it from `schema.rb`.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150515152820) do
+ActiveRecord::Schema.define(:version => 20150603234557) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "namespace"
@@ -473,7 +473,6 @@ ActiveRecord::Schema.define(:version => 20150515152820) do
     t.datetime "updated_at",                                                                                                                                                                                                                                      :null => false
     t.integer  "population"
     t.boolean  "public_register",      :default => true
-    t.boolean  "free",                 :default => true
     t.boolean  "register_form"
     t.text     "welcom_message"
     t.string   "image_front"
