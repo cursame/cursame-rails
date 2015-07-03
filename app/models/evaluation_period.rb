5dbb614ab790d06268f8849e09b6b334419477aa
lruiz05 <luis05021990@gmail.com>
2015-07-03 14:50:24 -0500

    Se crean los criterios de evaluacion para un curso de bit.
    Al importar un curso de bit, se crean para cada periodo de evaluación de bit
    los criterios de cursame (Tareas, Cuestionarios, Discusiones).

--- app/models/evaluation_period.rb
+++ app/models/evaluation_period.rb
@@ -2,7 +2,19 @@ class EvaluationPeriod < ActiveRecord::Base
   attr_accessible :course_id, :name, :order, :short_name
 
   belongs_to :course
-  validates_associated :course, presence: true
+  validates_associated :course
 
   has_many :evaluation_criteria, as: :evaluable, dependent: :destroy
+
+  attr_accessible :evaluation_criteria_attributes
+
+  accepts_nested_attributes_for :evaluation_criteria, allow_destroy: true
+
+  def new_evaluation_criteria
+    if evaluation_criteria.blank?
+      EvaluationCriterium.create(name: 'cursame_deliveries', evaluable: self, evaluation_percentage: 0)
+      EvaluationCriterium.create(name: 'cursame_surveys', evaluable: self, evaluation_percentage: 0)
+      EvaluationCriterium.create(name: 'cursame_discussions', evaluable: self, evaluation_percentage: 0)
+    end
+  end
 end
