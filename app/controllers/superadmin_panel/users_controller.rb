ef39c9fe9dc21ae7ace2dbdc2f36c7a217757448
José Emiliano Cabrera Blancas <jemiliano.cabrera@gmail.com>
2015-08-26 14:46:48 -0500

    Added view for index users with its locales

--- app/controllers/superadmin_panel/users_controller.rb
+++ app/controllers/superadmin_panel/users_controller.rb
@@ -1,7 +1,6 @@
 class SuperadminPanel::UsersController < SuperadminPanel::BaseController
-
   def index
-    @users = User.includes(permissionings: [:network, :role]).
-             order(:email).paginate(page: params[:page], per_page: 30)
+    @users = User.includes(permissionings: [:network, :role])
+             .order(:email).paginate(page: params[:page], per_page: 30)
   end
 end
