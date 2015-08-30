3b1395641687ca1909992a80b4ef94e67f0a01b7
José Emiliano Cabrera Blancas <jemiliano.cabrera@gmail.com>
2015-08-30 01:31:32 -0500

    Added edit, update, create, new and set_params to networks controller.

--- app/controllers/superadmin_panel/networks_controller.rb
+++ app/controllers/superadmin_panel/networks_controller.rb
@@ -3,7 +3,45 @@ class SuperadminPanel::NetworksController < SuperadminPanel::BaseController
     @networks = Network.order(:subdomain).paginate(page: params[:page], per_page: 30)
   end
 
+  def new
+    @network = Network.new
+  end
+
+  def create
+    set_params(nil)
+  end
+
+  def edit
+    @network = Network.find_by_id params[:id]
+  end
+
+  def update
+    set_params(params[:id])
+  end
+
   def show
     @network = Network.find_by_id params[:id]
+  end
+
+  private
+
+  def set_params(id)
+    if id
+      network = Network.find_by_id(id)
+    else
+      network = Network.new params[:network]
+    end
+
+    if !id && !network.save
+      redirect_to superadmin_panel_networks_path, flash: { error: 'error' }
+      return
+    end
+
+    if id && !network.update_attributes(params[:network])
+      redirect_to superadmin_panel_networks_path, flash: { error: 'error' }
+      return
+    end
+
+    redirect_to superadmin_panel_network_path(network), flash: { success: 'success' }
   end
 end
