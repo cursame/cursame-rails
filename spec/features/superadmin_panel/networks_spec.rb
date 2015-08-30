c19643db5ba983a774413b0fe6e0033e6eebc44e
José Emiliano Cabrera Blancas <jemiliano.cabrera@gmail.com>
2015-08-30 01:30:39 -0500

    Added tests for update and edit networks.

--- spec/features/superadmin_panel/networks_spec.rb
+++ spec/features/superadmin_panel/networks_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.feature 'Networks Views:' do
   background do
-    @network = FactoryGirl.create(:network)
+    @network_testing = FactoryGirl.create(:network)
+    @network = FactoryGirl.create(:network, subdomain: Faker::Internet.domain_word)
+
     @superadmin = FactoryGirl.create(:user, role: "superadmin")
     given_logged_in_as(@superadmin)
     visit "/superadmin_panel/networks"
@@ -10,16 +12,34 @@ RSpec.feature 'Networks Views:' do
 
   scenario "Get #index" do
     click_on I18n.t('superadmin_panel.shared.sidebar.networks')
-    expect(page).to have_css("table.data-table tr", count: 2)
+    expect(page).to have_css("table.data-table tr", count: 3)
   end
 
   scenario "Get #show" do
     locale_root = 'superadmin_panel.networks'
-    find(".networks-id-#{@network.id}").click_on I18n.t("#{locale_root}.index.view")
-    expect(page).to have_text @network.name
-    expect(page).to have_text @network.subdomain
-    expect(page).to have_text @network.population
-    expect(page).to have_text @network.welcom_message if @network.welcom_message
-    expect(page).to have_text I18n.t("#{locale_root}.show.#{@network.public_register?}")
+    find(".networks-id-#{@network_testing.id}").click_on I18n.t("#{locale_root}.index.view")
+
+    expect_network_show_texts(@network_testing)
+  end
+
+  scenario "post #create" do
+    click_on I18n.t('superadmin_panel.networks.index.create')
+
+    network = FactoryGirl.build(:network, subdomain: Faker::Internet.domain_word)
+    fill_network_form(network)
+
+    click_on 'action-button'
+    expect_network_show_texts(network)
+  end
+
+  scenario "put #update" do
+    locale_root = 'superadmin_panel.networks'
+    find(".networks-id-#{@network.id}").click_on I18n.t("#{locale_root}.index.edit")
+
+    network = FactoryGirl.build(:network, subdomain: Faker::Internet.domain_word)
+    fill_network_form(network)
+
+    click_on 'action-button'
+    expect_network_show_texts(network)
   end
 end
