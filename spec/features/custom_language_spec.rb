e2e1d40d19d497bd806f859b342a3e2c815ebe57
Isaac Pompa <isaacsalvador18@gmail.com>
2015-08-31 14:37:17 -0500

    Spec for the Network sign in view
    * Factored in method verify_lang.

--- spec/features/custom_language_spec.rb
+++ spec/features/custom_language_spec.rb
@@ -3,30 +3,26 @@ require 'rails_helper'
 RSpec.feature 'Load the custom language in network' do
 
   scenario 'The platform displays in the :en language' do
-    net = FactoryGirl.create(:network, language: 'en')
-    user = create(:user)
-    given_logged_in_as(user)
-    visit root_path
-    lang = I18n.locale.to_s
-    expect(lang).to eq("en")
+    verify_lang('en')
   end
 
   scenario 'The platform displays in the :es language' do
-    net = FactoryGirl.create(:network, language: 'es')
-    user = create(:user)
-    given_logged_in_as(user)
-    visit root_path
-    lang = I18n.locale.to_s
-    expect(lang).to eq("es")
+    verify_lang('es')
   end
 
-  scenario 'The platform dont displays in the :en language' do
-    net = FactoryGirl.create(:network, language: 'es')
+  scenario 'Display the sign_in view in the prefered language' do
+    net = create(:network, language: 'en')
+    visit sign_in_path
+    expect(page).to have_content 'Log in'
+  end
+
+  def verify_lang(language)
+    net = FactoryGirl.create(:network, language: language)
     user = create(:user)
     given_logged_in_as(user)
     visit root_path
     lang = I18n.locale.to_s
-    expect(lang).not_to be == "en"
+    expect(lang).to eq(language)
   end
 end
 
