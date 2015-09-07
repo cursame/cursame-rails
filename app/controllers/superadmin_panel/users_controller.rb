0ac31b0396d47e78351df2cb95fb55ced284acd2
José Emiliano Cabrera Blancas <jemiliano.cabrera@gmail.com>
2015-09-07 15:32:41 -0500

    Added friendly error messages to create/edit networks.

--- app/controllers/superadmin_panel/users_controller.rb
+++ app/controllers/superadmin_panel/users_controller.rb
@@ -1,4 +1,5 @@
 class SuperadminPanel::UsersController < SuperadminPanel::BaseController
+
   def index
     @users = User.includes(permissionings: [:network, :role])
              .order(:email).paginate(page: params[:page], per_page: 30)
