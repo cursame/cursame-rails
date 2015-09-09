05faf6b70d9c3fd0e7d156525b83fcb8d41e0d0c
José Emiliano Cabrera Blancas <jemiliano.cabrera@gmail.com>
2015-09-09 13:27:19 -0500

    Added friendly error messages to create/edit users.

--- spec/features/superadmin_panel/users_spec.rb
+++ spec/features/superadmin_panel/users_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.feature 'Users Views:' do
   background do
-    FactoryGirl.create(:network)
+    @network = FactoryGirl.create(:network)
+
     @superadmin = FactoryGirl.create(:user, role: "superadmin")
     @user = FactoryGirl.create(:user, role: %w(admin student operator teacher).sample)
     @mentor_link = FactoryGirl.create(:user, role: "mentor_link")
@@ -31,18 +32,31 @@ RSpec.feature 'Users Views:' do
 
     user = FactoryGirl.build(:user)
     role_title = %w(superadmin admin teacher student).sample
-    fill_user_form(user, role_title, 'testing')
+    fill_user_form(user, role_title, @network)
 
     click_on 'action-button'
     expect_user_show_texts(user, role_title)
   end
 
+  scenario "post #create with invalid email and password" do
+    click_on I18n.t('superadmin_panel.users.index.create')
+
+    user = FactoryGirl.build(:user)
+    user.email = @user.email
+    role_title = %w(superadmin admin teacher student).sample
+    fill_user_form(user, role_title, @network)
+    fill_in('user_password', with: '12345')
+
+    click_on 'action-button'
+    expect(page).to have_text(I18n.t('activerecord.errors.messages.taken'))
+  end
+
   scenario "post #create mentor_link" do
     click_on I18n.t('superadmin_panel.users.index.create')
 
-    FactoryGirl.create(:network, subdomain: 'meems')
+    meems = FactoryGirl.create(:network, subdomain: 'meems')
     user = FactoryGirl.build(:user)
-    entity = fill_user_form(user, "mentor_link", 'meems')
+    entity = fill_user_form(user, "mentor_link", meems)
 
     click_on 'action-button'
     expect(page).to have_text entity[:id]
@@ -55,7 +69,7 @@ RSpec.feature 'Users Views:' do
     user = FactoryGirl.build(:user)
     user.email = @user.email
     role_title = %w(superadmin admin teacher student).sample
-    fill_user_form(user, role_title, 'testing')
+    fill_user_form(user, role_title, @network)
 
     click_on 'action-button'
     expect_user_show_texts(user, role_title)
@@ -64,10 +78,10 @@ RSpec.feature 'Users Views:' do
   scenario "put #update mentor_link" do
     find(".user-id-#{@user.id}").click_on I18n.t('superadmin_panel.users.index.edit')
 
-    FactoryGirl.create(:network, subdomain: 'meems')
+    meems = FactoryGirl.create(:network, subdomain: 'meems')
     user = FactoryGirl.build(:user)
     user.email = @user.email
-    entity = fill_user_form(user, 'mentor_link', 'meems')
+    entity = fill_user_form(user, 'mentor_link', meems)
 
     click_on 'action-button'
     expect(page).to have_text entity[:id].to_i
@@ -82,20 +96,20 @@ RSpec.feature 'Users Views:' do
     expect(page).to have_text user.subdomain
   end
 
-  def fill_user_form(user, role_title, network_subdomain)
+  def fill_user_form(user, role_title, network)
     fill_in('user_email', with: user.email)
     fill_in('user_first_name', with: user.first_name)
     fill_in('user_last_name', with: user.last_name)
     fill_in('user_password', with: 'password')
-    fill_in('user_network_subdomain', with: network_subdomain)
+    fill_in('user_permissionings_attributes_0_network_id', with: network.id)
 
-    select(I18n.t("roles.#{role_title}"), from: 'user_role_id')
+    select(I18n.t("roles.#{role_title}"), from: 'user_permissionings_attributes_0_role_id')
 
     if (role_title == "mentor_link")
       entity = { id: Faker::Number.number(2), name: Faker::Lorem.word }
 
-      fill_in('user_entity_id', with: entity[:id])
-      fill_in('user_entity_name', with: entity[:name])
+      fill_in('user_permissionings_attributes_0_entity_id', with: entity[:id])
+      fill_in('user_permissionings_attributes_0_entity_name', with: entity[:name])
 
       entity
     end
