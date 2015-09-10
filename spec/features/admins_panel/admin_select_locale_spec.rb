14c97922fbda3cb722a103cca2117f9b21934734
Isaac Pompa <isaacsalvador18@gmail.com>
2015-09-10 18:45:03 -0500

    Option to enable the user language platform
    * Spec
    * Add the check box in network config view

--- spec/features/admins_panel/admin_select_locale_spec.rb
+++ spec/features/admins_panel/admin_select_locale_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature 'Select locale for network' do
+RSpec.feature 'Select language for network' do
   background do
     FactoryGirl.create(:network)
     @admin = FactoryGirl.create(:user, role: 'admin')
@@ -12,6 +12,10 @@ RSpec.feature 'Select locale for network' do
     expect(page).to have_content I18n.t('networks.form.change_locale') #change locale
     expect(page).to have_select('network_language',
       with_options: languages)
+  end
+
+  scenario 'Enable multilanguage for users' do
+    page.check(I18n.t('networks.form.enable_multilanguage'))
   end
 
   def languages
