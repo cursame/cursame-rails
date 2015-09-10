26444288bd9735e9a7a4147a0037a35f971181dc
Isaac Pompa <isaacsalvador18@gmail.com>
2015-09-10 18:32:28 -0500

    Associations for User and UserLanguage models
    * attr_accesible in UserLanguage
    * nested_atrributes in User

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ActiveRecord::Base
     :friendships, :friends, :registerable, :image_avatarx,
     :image_avatarxx, :cover_photox, :confirmation_token, :locked_at,
     :tour_info,:activities, :accepted_terms, :confirmed_at, :subdomain,
-    :domain, :permissioning_ids, :self_register
+    :domain, :permissioning_ids, :self_register, :user_language_attributes
 
   has_many :friendships, :uniq => true, :dependent => :destroy
   has_many :inverse_friendships, :class_name => "Friendship",
@@ -69,6 +69,7 @@ class User < ActiveRecord::Base
   accepts_nested_attributes_for :assets
   accepts_nested_attributes_for :permissionings
   accepts_nested_attributes_for :networks
+  accepts_nested_attributes_for :user_language
 
   #comentarios para usuarios
   acts_as_commentable
--- app/models/user_language.rb
+++ app/models/user_language.rb
@@ -1,4 +1,5 @@
 class UserLanguage < ActiveRecord::Base
+  attr_accessible :user_id, :language_id
   belongs_to :user
   belongs_to :language
 
