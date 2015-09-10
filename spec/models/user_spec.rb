fc52b4206fcb4f005bc061b116cee3504adca0c7
Isaac Pompa <isaacsalvador18@gmail.com>
2015-09-10 18:38:47 -0500

    Spec for the User attr mass_assignment

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  describe 'User' do
+  describe 'User associations' do
     it { should have_one(:user_language) }
     it { should have_one(:language).through(:user_language) }
+    it { should accept_nested_attributes_for :user_language }
+    it { should allow_mass_assignment_of :user_language_attributes }
   end
 end
