d77077c97af4f601df8a8b95fb3da47264092967
José Emiliano Cabrera Blancas <jemiliano.cabrera@gmail.com>
2015-10-04 20:08:10 -0500

    Replace the same constructions in every method for an auxiliary method ```options_for_select_roles``` in ```app/helpers/roles_helper.rb```.

--- app/helpers/roles_helper.rb
+++ app/helpers/roles_helper.rb
@@ -1,2 +1,6 @@
 module RolesHelper
+  def options_for_select_roles(user)
+    roles_options = Role.all.map { |role| [ I18n.t("roles.#{role.title}"), role.id] }
+    options_for_select(roles_options, user.permissionings.first.role_id)
+  end
 end
