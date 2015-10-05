e655c2f403355b80f02cad5cd0d8adaa8f4f71a1
José Emiliano Cabrera Blancas <jemiliano.cabrera@gmail.com>
2015-10-05 13:41:40 -0500

    Added migrations to insert ```catalogs``` schema into the database.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150903193002) do
+ActiveRecord::Schema.define(:version => 20150930195006) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "namespace"
@@ -141,6 +141,14 @@ ActiveRecord::Schema.define(:version => 20150903193002) do
   end
 
   add_index "bit_settings", ["network_id"], :name => "index_bit_settings_on_network_id"
+
+  create_table "catalogs", :force => true do |t|
+    t.string   "csv_file",   :limit => 500
+    t.string   "status",     :limit => 10,  :default => "pending"
+    t.string   "domain"
+    t.datetime "created_at",                                       :null => false
+    t.datetime "updated_at",                                       :null => false
+  end
 
   create_table "channels", :force => true do |t|
     t.string   "channel_type"
@@ -464,7 +472,7 @@ ActiveRecord::Schema.define(:version => 20150903193002) do
     t.datetime "updated_at",              :null => false
   end
 
-  add_index "members_in_course_criteria", ["evaluation_criterium_id"], :name => "index_members_in_course_criteria_on_evaluation_criterium_id"
+  add_index "members_in_course_criteria", ["evaluation_criterium_id"], :name => "member_criterium_i"
   add_index "members_in_course_criteria", ["members_in_course_id"], :name => "index_members_in_course_criteria_on_members_in_course_id"
 
   create_table "members_in_courses", :force => true do |t|
@@ -503,16 +511,17 @@ ActiveRecord::Schema.define(:version => 20150903193002) do
     t.datetime "updated_at",                                                                                                                                                                                                                                      :null => false
     t.integer  "population"
     t.boolean  "public_register",      :default => true
-    t.boolean  "register_form"
+    t.boolean  "register_form",        :default => false
     t.text     "welcom_message"
-    t.string   "image_front"
-    t.string   "logo"
-    t.string   "logo_type"
+    t.string   "image_front",          :default => "background-restore.jpg"
+    t.string   "logo",                 :default => "logo.png"
+    t.string   "logo_type",            :default => "128x26"
     t.text     "titles",               :default => "user: Usuario, profesor: Maestro, student: Alumno, admin: Administrador, course: Curso, courses: Cursos, friend: Amigo, friends: Amigos, comunity: Comunidad, students: Estudiantes, profesores: Profesores"
     t.string   "personalize_domain"
     t.boolean  "authenticate_teacher"
     t.boolean  "evaluable",            :default => true
-    t.string   "language"
+    t.string   "language",             :default => "es"
+    t.boolean  "multilanguage",        :default => false
   end
 
   create_table "networks_users", :force => true do |t|
@@ -530,13 +539,6 @@ ActiveRecord::Schema.define(:version => 20150903193002) do
     t.datetime "created_at",                         :null => false
     t.datetime "updated_at",                         :null => false
     t.boolean  "active",           :default => true
-  end
-
-  create_table "p_id_to_h_ids", :force => true do |t|
-    t.integer  "p_id"
-    t.integer  "h_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
   end
 
   create_table "permissionings", :force => true do |t|
@@ -620,7 +622,7 @@ ActiveRecord::Schema.define(:version => 20150903193002) do
     t.integer  "evaluation_criterium_id"
   end
 
-  add_index "response_to_the_evaluations", ["evaluation_criterium_id"], :name => "index_response_to_the_evaluations_on_evaluation_criterium_id"
+  add_index "response_to_the_evaluations", ["evaluation_criterium_id"], :name => "criterium_index"
   add_index "response_to_the_evaluations", ["feedbackable_id", "feedbackable_type"], :name => "feedbackable_index"
 
   create_table "role_id_and_permission_ids", :force => true do |t|
@@ -636,8 +638,6 @@ ActiveRecord::Schema.define(:version => 20150903193002) do
     t.date     "created"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
-    t.string   "entity_name"
-    t.integer  "entity_id"
   end
 
   create_table "schools", :force => true do |t|
