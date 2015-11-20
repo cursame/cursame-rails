5b0068bdc8d58812061ffe0a55c6c9bfd8569a3e
Isaac Pompa <isaacsalvador18@gmail.com>
2015-11-20 12:27:42 -0600

    Spec when the library file dont upload by the same user
    * Rename the scenarios for correct description of them

--- spec/features/library_file/user_deletes_library_file_spec.rb
+++ spec/features/library_file/user_deletes_library_file_spec.rb
@@ -4,11 +4,17 @@ RSpec.feature 'Delete a library file'  do
   background do
     @network = create(:network)
     @user = create(:user, role: 'teacher')
+    @not_owner_user = create(:user)
   end
 
-  scenario 'File deleted succesfully' do
+  scenario 'When the user upload a file, then deleted succesfully' do
     upload_library_file
     delete_library_file
+  end
+
+  scenario 'If the user dont upload the file, then do not delete them' do
+    upload_library_file
+    user_doesnt_delete_file
   end
 
   def upload_library_file
@@ -25,5 +31,11 @@ RSpec.feature 'Delete a library file'  do
     visit library_path(@network.library)
     click_link I18n.t('library_files.shared.standalone_card.delete_file')
     expect(page).to have_content I18n.t('library_directories.shared.list.no_documents')
+  end
+
+  def user_doesnt_delete_file
+    given_logged_in_as(@not_owner_user)
+    visit library_path(@network.library)
+    expect(page).to have_no_content I18n.t('library_files.shared.standalone_card.delete_file')
   end
 end
