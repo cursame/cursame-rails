f840b5a11a6b90a01c12c3fe152bedc555877c4d
Isaac Pompa <isaacsalvador18@gmail.com>
2015-12-01 18:24:32 -0600

    Add create_course helper method

--- spec/support/features_helper_methods.rb
+++ spec/support/features_helper_methods.rb
@@ -6,5 +6,11 @@ module Features
       fill_in("user_password", with: 'password')
       click_on("sign_in")
     end
+
+    def create_course(teacher, network)
+      course = create(:course, network_id: network.id)
+      create(:members_in_course, user_id: teacher.id, course_id: course.id, accepted: true, owner: true, network_id: network.id)
+      course
+    end
   end
 end
